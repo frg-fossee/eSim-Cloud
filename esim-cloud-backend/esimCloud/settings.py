bb025ae206ea3979729c6b7c20a2576258ad128c
Darsh Patel <darshkpatel@gmail.com>
2020-04-26 15:12:12 +0530

    Remove redundant env variables

--- esim-cloud-backend/esimCloud/settings.py
+++ esim-cloud-backend/esimCloud/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'esimCloud.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "NAME": os.environ.get("MYSQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("MYSQL_USER", "user"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     },
