a6399dedafc2dde6b69609eba59c7a17a9eccade
Navonil Das <navneeladas@gmail.com>
2020-04-26 12:26:29 +0530

    Added Circuit Element Class

--- ArduinoFrontend/src/app/simulator/simulator.component.ts
+++ ArduinoFrontend/src/app/simulator/simulator.component.ts
@@ -8,7 +8,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SimulatorComponent implements OnInit {
 
-  constructor(private aroute: ActivatedRoute) { }
+  constructor(private aroute: ActivatedRoute) {
+    // Stores all the Circuit Information
+    window['scope'] = {
+    };
+    // True when simulation takes place
+    window['isSimulating'] = false;
+    // Stores the reference to the selected circuit component
+    window['selected'] = null;
+    // True when a component is selected
+    window['isSelected'] = false;
+  }
 
   ngOnInit() {
     this.aroute.queryParams.subscribe(v => {
