f69d4c15aaab0c240de7df9335103ecf2328a118
Darsh Patel <darshkpatel@gmail.com>
2020-04-27 23:52:27 +0530

    Github Actions (#19)
    * GIthub Action docker-compose
    
    Github Action for dev containers
    
    * Action to run django tests
    
    * Fix pep8 warnings to trigger github Action
    
    * Reduce number of builds
    
    * Django Tests

--- esim-cloud-backend/esimCloud/settings.py
+++ esim-cloud-backend/esimCloud/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", 'kk5tq+=kyyicitl+1ki!wyx@*mz^vmei6_q25dt!^3(_kxd^eg')
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 'kk5tq+=kyyicitl+1ki!wyx@*mz^vmei6_q25dt!^3(_kxd^eg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=True))
@@ -71,19 +72,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'esimCloud.wsgi.application'
 
 
-# Database config
-#Defaults to sqlite3 if not provided in environment files
+# Database config Defaults to sqlite3 if not provided in environment files
 
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("MYSQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "NAME": os.environ.get("MYSQL_DATABASE",
+                               os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("MYSQL_USER", "user"),
         "PASSWORD": os.environ.get("MYSQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     },
-    "mongodb":{
+    "mongodb": {
         "ENGINE": 'djongo',
         "NAME": os.environ.get("MONGO_INITDB_DATABASE", "esimcloud_db"),
         "USER": os.environ.get("MONGO_INITDB_ROOT_USERNAME", "user"),
@@ -99,16 +100,16 @@ DATABASES = {
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',    # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',              # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',             # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',            # noqa
     },
 ]
 
