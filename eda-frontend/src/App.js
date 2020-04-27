a4757a0d9a8f8084fa43e8ab6d333e01ed38e2eb
dssudake <darshansudake555@gmail.com>
2020-04-27 13:40:25 +0530

    Added Home page

--- eda-frontend/src/App.js
+++ eda-frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
