99002ba0eab3781e0fe91c5e1c1026be1e83175d
Darsh <darshkpatel@gmail.com>
2020-04-30 20:28:07 +0530

    HotFix: URL for docs

--- esim-cloud-backend/esimCloud/urls.py
+++ esim-cloud-backend/esimCloud/urls.py
@@ -32,12 +32,12 @@ urlpatterns = [
 
 
     # For API Documentation
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
+    url(r'^api/docs(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(
             cache_timeout=0),
         name='schema-json'),
 
-    url(r'^swagger/$', schema_view.with_ui(
+    path('api/docs', schema_view.with_ui(
         'swagger',
         cache_timeout=0),
         name='schema-swagger-ui'),
