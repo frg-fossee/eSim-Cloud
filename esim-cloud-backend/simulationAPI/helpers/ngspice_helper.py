20a568c314bde8609e265e71ff9df59308a12fba
Darsh <darshkpatel@gmail.com>
2020-04-30 20:09:57 +0530

    Switch to logger

--- esim-cloud-backend/simulationAPI/helpers/ngspice_helper.py
+++ esim-cloud-backend/simulationAPI/helpers/ngspice_helper.py
@@ -1,8 +1,10 @@
 import os
+import logging
 import subprocess
 from pathlib import Path
 from django.conf import settings
 from .parse import extract_data_from_ngspice_output
+logger = logging.getLogger(__name__)
 
 
 class CannotAppendPlot(Exception):
@@ -21,39 +23,38 @@ def ExecNetlist(filepath, file_id):
     try:
 
         current_dir = settings.MEDIA_ROOT+'/'+str(file_id)
-        print("Workdir: ", current_dir)
         # Make Unique Directory for simulation to run
         Path(current_dir).mkdir(parents=True, exist_ok=True)
-        print('will run ngSpice command')
+        logger.info('will run ngSpice command')
         proc = subprocess.Popen(['ngspice', '-ab', filepath],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                 cwd=current_dir)
         stdout, stderr = proc.communicate()
-        print('Ran ngSpice command')
+        logger.info('Ran ngSpice command')
 
         if proc.returncode not in [0, 1]:
-            print('ngspice error encountered')
-            print(stderr)
-            print(proc.returncode)
-            print(stdout)
+            logger.error('ngspice error encountered')
+            logger.error(stderr)
+            logger.error(proc.returncode)
+            logger.error(stdout)
             target = os.listdir(current_dir)
             for item in target:
                 if (item.endswith(".txt")):
                     os.remove(os.path.join('.', item))
             raise CannotRunSpice("ngspice exited with error")
         else:
-            print('Ran ngSpice')
+            logger.info('Ran ngSpice')
 
-        print("Reading Output")
+        logger.info("Reading Output")
         output = extract_data_from_ngspice_output(current_dir+'/data.txt')
         return output
     except Exception as e:
-        print('Encountered Exception:')
-        print(e)
+        logger.exception('Encountered Exception:')
+        logger.exception(e)
     finally:
         target = os.listdir(current_dir)
         os.remove(filepath)
         for item in target:
             os.remove(os.path.join(current_dir, item))
         os.rmdir(current_dir)
-        print('Deleted Files')
+        logger.info('Deleted Files')
