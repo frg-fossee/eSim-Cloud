e14478a391e184eb7386961c50b469e0dc81dafe
dssudake <darshansudake555@gmail.com>
2020-05-06 03:38:56 +0530

    Header for user Dashboard

--- eda-frontend/src/pages/Dashboard.js
+++ eda-frontend/src/pages/Dashboard.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Layout from "../components/Shared/Layout";
+import Header from "../components/User/Header";
 
 export default function Dashboard() {
   return (
     <>
-      <Layout />
+      <Layout resToolbar={<Header />} />
     </>
   );
 }
