726722465b34829edba9374d20d4b93998cdefc1
dssudake <darshansudake555@gmail.com>
2020-05-06 02:13:02 +0530

    Modified Header of Schematic Editor

--- eda-frontend/src/pages/SchematiEditor.js
+++ eda-frontend/src/pages/SchematiEditor.js
@@ -1,24 +1,11 @@
 import React from "react";
 
-import { Container, Typography } from "@material-ui/core";
 import Header from "../components/SchematicEditor/Header";
 
 export default function SchematiEditor() {
   return (
     <>
       <Header />
-
-      <Container maxWidth="lg">
-        <Typography
-          variant="h3"
-          align="center"
-          color="textSecondary"
-          gutterBottom
-        >
-          <br></br>
-          Schematic Editor
-        </Typography>
-      </Container>
     </>
   );
 }
