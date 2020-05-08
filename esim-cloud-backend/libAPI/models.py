d16e3ead384de7cd4be88883366ec1a85d288571
Darsh <darshkpatel@gmail.com>
2020-05-08 18:33:21 +0530

    models

--- esim-cloud-backend/libAPI/models.py
+++ esim-cloud-backend/libAPI/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class LibraryComponent(models.Model):
+    component_name = models.CharField(max_length=200)
+    svg_path = models.FilePathField()
+    component_type = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.component_name
+
+
+class Library(models.Model):
+    library_name = models.CharField(max_length=200)
+    library_type = models.CharField(max_length=20)
+    saved_on = models.DateTimeField(auto_now=True)
+    components = models.ArrayField(
+        model_container=LibraryComponent,
+    )
+
+    def __str__(self):
+        return self.library_name
