c5e8a65e488815487658f69adf9a23fbc0f9f178
dssudake <darshansudake555@gmail.com>
2020-05-09 16:22:58 +0530

    Added Drop component feature for Grid

--- eda-frontend/src/components/SchematicEditor/SchematicGrid.js
+++ eda-frontend/src/components/SchematicEditor/SchematicGrid.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
 import "./Helper/SchematicEditor.css";
+import LoadGrid from "./Helper/EditerDrop.js";
 
 export default class SchematicGrid extends Component {
+  componentDidMount() {
+    var container = ReactDOM.findDOMNode(this.refs.divGrid);
+    LoadGrid(container);
+  }
+
   render() {
     return <div className="grid-container" ref="divGrid" id="divGrid" />;
   }
