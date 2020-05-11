c1a93505d8a53a7f725a8c2b978bcd8cd0bacc73
Navonil Das <navneeladas@gmail.com>
2020-05-11 12:06:11 +0530

    Sepration For Gas Sensor

--- ArduinoFrontend/src/app/Libs/inputs/GasSensor.ts
+++ ArduinoFrontend/src/app/Libs/inputs/GasSensor.ts
@@ -3,21 +3,8 @@ import { Point } from '../Point';
 
 
 export class MQ2 extends CircuitElement {
-  static pointHalf = 4;
   constructor(public canvas: any, x: number, y: number) {
-    super('MQ2', x, y);
-    this.elements.push(
-      this.canvas.image('assets/images/components/GasSensor.svg', this.x, this.y, 96, 170),
-    );
-    this.nodes = [
-      new Point(canvas, x + 24, y + 160, 'VCC', MQ2.pointHalf, this),
-      new Point(canvas, x + 36, y + 160, 'GND', MQ2.pointHalf, this),
-      new Point(canvas, x + 49, y + 160, 'D0', MQ2.pointHalf, this),
-      new Point(canvas, x + 61, y + 160, 'A0', MQ2.pointHalf, this),
-    ];
-    this.setClickListener(null);
-    this.setDragListeners();
-    this.setHoverListener();
+    super('MQ2', x, y, 'MQ2.json', canvas);
   }
   save() {
   }
