9cd5ad1aeaa56ee3b1c15d31b973161964ef93be
Navonil Das <navneeladas@gmail.com>
2020-05-11 12:37:41 +0530

    Sepration of Ultrasonic Sensor

--- ArduinoFrontend/src/app/Libs/inputs/UltrasonicSensor.ts
+++ ArduinoFrontend/src/app/Libs/inputs/UltrasonicSensor.ts
@@ -2,23 +2,8 @@ import { CircuitElement } from '../CircuitElement';
 import { Point } from '../Point';
 
 export class UltrasonicSensor extends CircuitElement {
-  static pointHalf = 4;
-
   constructor(public canvas: any, public x: number, y: number) {
-    super('UltrasonicSensor', x, y);
-
-    this.elements.push(
-      this.canvas.image('assets/images/components/UltrasonicSensor.svg', this.x, this.y, 203, 103.5)
-    );
-    this.nodes = [
-      new Point(canvas, x + 78, y + 100, 'VCC', UltrasonicSensor.pointHalf, this),
-      new Point(canvas, x + 88, y + 100, 'TRIG', UltrasonicSensor.pointHalf, this),
-      new Point(canvas, x + 100, y + 100, 'ECHO', UltrasonicSensor.pointHalf, this),
-      new Point(canvas, x + 110, y + 100, 'GND', UltrasonicSensor.pointHalf, this),
-    ];
-    this.setDragListeners();
-    this.setClickListener(null);
-    this.setHoverListener();
+    super('UltrasonicSensor', x, y, 'UltrasonicSensor.json', canvas);
   }
   save() {
   }
