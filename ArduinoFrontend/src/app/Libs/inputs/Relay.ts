79431007d37cc0c3cf9908429d6779a11441af7f
Navonil Das <navneeladas@gmail.com>
2020-05-11 12:56:39 +0530

    Seperation of Relay

--- ArduinoFrontend/src/app/Libs/inputs/Relay.ts
+++ ArduinoFrontend/src/app/Libs/inputs/Relay.ts
@@ -5,22 +5,7 @@ export class Relay extends CircuitElement {
   static pointHalf = 5;
 
   constructor(public canvas: any, public x: number, y: number) {
-    super('RelayModule', x, y);
-
-    this.elements.push(
-      this.canvas.image('assets/images/components/1ChannelRelay.svg', this.x, this.y, 118, 204)
-    );
-    this.nodes = [
-      new Point(canvas, x + 33, y + 18, 'Normally Closed', Relay.pointHalf, this),
-      new Point(canvas, x + 56, y + 18, 'COMMON', Relay.pointHalf, this),
-      new Point(canvas, x + 81, y + 18, 'Normally Open', Relay.pointHalf, this),
-      new Point(canvas, x + 54, y + 197, 'SIG', Relay.pointHalf, this),
-      new Point(canvas, x + 68, y + 197, 'VCC', Relay.pointHalf, this),
-      new Point(canvas, x + 84, y + 197, 'GND', Relay.pointHalf, this),
-    ];
-    this.setDragListeners();
-    this.setClickListener(null);
-    this.setHoverListener();
+    super('RelayModule', x, y, 'Relay.json', canvas);
   }
   save() {
   }
