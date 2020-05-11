3735e8f4ace787be5ba068dc828f0032e72413eb
Navonil Das <navneeladas@gmail.com>
2020-05-11 12:35:14 +0530

    Sepration of Potentionmeter

--- ArduinoFrontend/src/app/Libs/inputs/Potentiometer.ts
+++ ArduinoFrontend/src/app/Libs/inputs/Potentiometer.ts
@@ -6,20 +6,7 @@ declare var Raphael;
 export class Potentiometer extends CircuitElement {
   static pointHalf = 5;
   constructor(public canvas: any, x: number, y: number) {
-    super('Potentiometer', x, y);
-    this.elements.push(
-      this.canvas.image('assets/images/components/Potentiometer.svg', this.x, this.y, 109.3, 122.975),
-      this.canvas.image('assets/images/components/PotentiometerAbove.svg', this.x + 13.3, this.y + 12.5, 82.6, 82.6),
-    );
-    this.nodes = [
-      new Point(canvas, x + 28, y + 114, 'Terminal 1', Potentiometer.pointHalf, this),
-      new Point(canvas, x + 51, y + 114, 'WIPER', Potentiometer.pointHalf, this),
-      new Point(canvas, x + 74, y + 114, 'Terminal 2', Potentiometer.pointHalf, this),
-    ];
-    this.setClickListener(null);
-    this.setDragListeners();
-    this.setHoverListener();
-
+    super('Potentiometer', x, y, 'Potentiometer.json', canvas);
     // const anim = Raphael.animation({transform: 'r360'}, 2500).repeat(Infinity);
     // this.elements[1].animate(anim);
   }
