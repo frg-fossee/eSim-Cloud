257b6fd855e8643499ae0246d34e0c87bc4363d1
Navonil Das <navneeladas@gmail.com>
2020-05-11 12:40:41 +0530

    Sepration of Temperature Sensor

--- ArduinoFrontend/src/app/Libs/inputs/TemperatureSensors.ts
+++ ArduinoFrontend/src/app/Libs/inputs/TemperatureSensors.ts
@@ -2,21 +2,8 @@ import { CircuitElement } from '../CircuitElement';
 import { Point } from '../Point';
 
 export class LM35 extends CircuitElement {
-  static pointHalf = 4;
   constructor(public canvas: any, x: number, y: number) {
-    super('LM35', x, y);
-    this.elements.push(
-      this.canvas.image('assets/images/components/LM35.svg', this.x, this.y, 29, 73),
-    );
-    this.nodes = [
-      new Point(canvas, x - 1, y + 64, 'Power', LM35.pointHalf, this),
-      new Point(canvas, x + 10, y + 64, 'Vout', LM35.pointHalf, this),
-      new Point(canvas, x + 22, y + 64, 'GND', LM35.pointHalf, this),
-    ];
-
-    this.setDragListeners();
-    this.setHoverListener();
-    this.setClickListener(null);
+    super('LM35', x, y, 'LM35.json', canvas);
   }
   save() {
   }
