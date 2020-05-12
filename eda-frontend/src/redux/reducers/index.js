5c46e1cf06e8dc1a922b3fc611633567d5f22a1f
Darsh <darshkpatel@gmail.com>
2020-05-12 17:39:49 +0530

    Linting Fixes

--- eda-frontend/src/redux/reducers/index.js
+++ eda-frontend/src/redux/reducers/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
 export default combineReducers({
 
--- eda-frontend/src/redux/store.js
+++ eda-frontend/src/redux/store.js
@@ -1,8 +1,8 @@
 
-import reducer from './reducers/index';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-const store = createStoreWithMiddleware(reducer);
+import reducer from './reducers/index'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
+const store = createStoreWithMiddleware(reducer)
 
 export default store
