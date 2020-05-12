fb077740a124bea7373ced25bd9cfe74a6e3a668
Darsh <darshkpatel@gmail.com>
2020-05-12 20:57:35 +0530

    Switch to Class based components

--- eda-frontend/src/components/SchematicEditor/ComponentSidebar.js
+++ eda-frontend/src/components/SchematicEditor/ComponentSidebar.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { Component } from 'react'
 import {
   Hidden,
   List,
@@ -17,6 +17,72 @@ import comp2 from '../../static/CircuitComp/C_1_A.svg'
 import comp3 from '../../static/CircuitComp/resistor.svg'
 import comp4 from '../../static/CircuitComp/Voltmeter_DC_1_A.svg'
 
+import { connect } from 'react-redux'
+import { fetchLibraries } from '../../redux/actions/index'
+
+class ComponentSidebar extends Component {
+  constructor (props) {
+    super(props)
+    this.setState({ open: false })
+
+    this.handleClick = (e) => {
+      this.setState({ open: !this.state.open })
+    }
+  }
+
+  render () {
+    return (
+      <>
+        <Hidden smDown>
+          <div className={useStyles.toolbar} />
+        </Hidden>
+
+        {/* Display List of categorized components */}
+        <List>
+          <ListItem button divider>
+            <h2 style={{ margin: '5px' }}>Components List</h2>
+          </ListItem>
+
+          <ListItem onClick={this.handleClick} button divider>
+            <span className={useStyles.head}>Sample Elements</span>
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding dense="true">
+              <ListItem divider>
+                <ListItemIcon>
+                  <img src={comp1} alt="Logo" />
+                </ListItemIcon>
+                <ListItemIcon>
+                  <img src={comp2} alt="Logo" />
+                </ListItemIcon>
+                <ListItemIcon>
+                  <img src={comp3} alt="Logo" />
+                </ListItemIcon>
+              </ListItem>
+
+              <ListItem divider>
+                <ListItemIcon>
+                  <img src={comp4} alt="Logo" />
+                </ListItemIcon>
+                <ListItemIcon>
+                  <img src={comp1} alt="Logo" />
+                </ListItemIcon>
+                <ListItemIcon>
+                  <img src={comp2} alt="Logo" />
+                </ListItemIcon>
+              </ListItem>
+            </List>
+          </Collapse>
+          <ListItem>
+            <div ref={this.props.compRef}></div>
+          </ListItem>
+        </List>
+      </>
+    )
+  }
+}
+
 const useStyles = makeStyles((theme) => ({
   toolbar: {
     minHeight: '90px'
@@ -30,62 +96,8 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-export default function ComponentSidebar (props) {
-  const classes = useStyles()
-
-  const [open, setOpen] = React.useState(false)
-
-  const handleClick = () => {
-    setOpen(!open)
-  }
-
-  return (
-    <>
-      <Hidden smDown>
-        <div className={classes.toolbar} />
-      </Hidden>
-
-      {/* Display List of categorized components */}
-      <List>
-        <ListItem button divider>
-          <h2 style={{ margin: '5px' }}>Components List</h2>
-        </ListItem>
-
-        <ListItem onClick={handleClick} button divider>
-          <span className={classes.head}>Sample Elements</span>
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding dense="true">
-            <ListItem divider>
-              <ListItemIcon>
-                <img src={comp1} alt="Logo" />
-              </ListItemIcon>
-              <ListItemIcon>
-                <img src={comp2} alt="Logo" />
-              </ListItemIcon>
-              <ListItemIcon>
-                <img src={comp3} alt="Logo" />
-              </ListItemIcon>
-            </ListItem>
-
-            <ListItem divider>
-              <ListItemIcon>
-                <img src={comp4} alt="Logo" />
-              </ListItemIcon>
-              <ListItemIcon>
-                <img src={comp1} alt="Logo" />
-              </ListItemIcon>
-              <ListItemIcon>
-                <img src={comp2} alt="Logo" />
-              </ListItemIcon>
-            </ListItem>
-          </List>
-        </Collapse>
-        <ListItem>
-          <div ref={props.compRef}></div>
-        </ListItem>
-      </List>
-    </>
-  )
-}
+// Redux Bindings
+const mapStateToProps = state => ({
+  libraries: state.schematicEditorReducer.libraries
+})
+export default connect(mapStateToProps, { fetchLibraries })(ComponentSidebar)
