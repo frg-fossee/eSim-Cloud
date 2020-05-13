b190553279769129806f77436c8b26727721a743
Darsh <darshkpatel@gmail.com>
2020-05-13 18:19:44 +0530

    Fetch Libraries and display

--- eda-frontend/src/components/SchematicEditor/ComponentSidebar.js
+++ eda-frontend/src/components/SchematicEditor/ComponentSidebar.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { Component } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import {
   Hidden,
   List,
@@ -15,73 +15,9 @@ import './Helper/SchematicEditor.css'
 import comp1 from '../../static/CircuitComp/4002_1_A.svg'
 import comp2 from '../../static/CircuitComp/C_1_A.svg'
 import comp3 from '../../static/CircuitComp/resistor.svg'
-import comp4 from '../../static/CircuitComp/Voltmeter_DC_1_A.svg'
 
-import { connect } from 'react-redux'
-import { fetchLibraries } from '../../redux/actions/index'
-
-class ComponentSidebar extends Component {
-  constructor (props) {
-    super(props)
-    this.setState({ open: false })
-
-    this.handleClick = (e) => {
-      this.setState({ open: !this.state.open })
-    }
-  }
-
-  render () {
-    return (
-      <>
-        <Hidden smDown>
-          <div className={useStyles.toolbar} />
-        </Hidden>
-
-        {/* Display List of categorized components */}
-        <List>
-          <ListItem button divider>
-            <h2 style={{ margin: '5px' }}>Components List</h2>
-          </ListItem>
-
-          <ListItem onClick={this.handleClick} button divider>
-            <span className={useStyles.head}>Sample Elements</span>
-            {open ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding dense="true">
-              <ListItem divider>
-                <ListItemIcon>
-                  <img src={comp1} alt="Logo" />
-                </ListItemIcon>
-                <ListItemIcon>
-                  <img src={comp2} alt="Logo" />
-                </ListItemIcon>
-                <ListItemIcon>
-                  <img src={comp3} alt="Logo" />
-                </ListItemIcon>
-              </ListItem>
-
-              <ListItem divider>
-                <ListItemIcon>
-                  <img src={comp4} alt="Logo" />
-                </ListItemIcon>
-                <ListItemIcon>
-                  <img src={comp1} alt="Logo" />
-                </ListItemIcon>
-                <ListItemIcon>
-                  <img src={comp2} alt="Logo" />
-                </ListItemIcon>
-              </ListItem>
-            </List>
-          </Collapse>
-          <ListItem>
-            <div ref={this.props.compRef}></div>
-          </ListItem>
-        </List>
-      </>
-    )
-  }
-}
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchLibraries, toggleCollapse } from '../../redux/actions/index'
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -95,9 +31,99 @@ const useStyles = makeStyles((theme) => ({
     marginRight: 'auto'
   }
 }))
+export default function ComponentSidebar (props) {
+  const classes = useStyles()
 
-// Redux Bindings
-const mapStateToProps = state => ({
-  libraries: state.schematicEditorReducer.libraries
-})
-export default connect(mapStateToProps, { fetchLibraries })(ComponentSidebar)
+  const [open, setOpen] = React.useState(false)
+  // const [collapse, setCollapse] = React.useState({ id: -1, open: false })
+
+  const handleClick = () => {
+    setOpen(!open)
+  }
+
+  const libraries = useSelector(state => state.schematicEditorReducer.libraries)
+  const collapse = useSelector(state => state.schematicEditorReducer.collapse)
+
+  const dispatch = useDispatch()
+
+  const handleCollapse = useCallback((id) => {
+    dispatch(toggleCollapse(id))
+    console.log(collapse)
+  })
+
+  // For Fetching Libraries
+  useEffect(() => {
+    dispatch(fetchLibraries())
+  }, [dispatch])
+
+  return (
+    <>
+      <Hidden smDown>
+        <div className={classes.toolbar} />
+      </Hidden>
+
+      {/* Display List of categorized components */}
+      <List>
+        <ListItem button divider>
+          <h2 style={{ margin: '5px' }}>Components List</h2>
+        </ListItem>
+
+        {/* Sample Collapsing List */}
+        <ListItem onClick={handleClick} button divider>
+          <span className={classes.head}>Sample Elements</span>
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding dense="true">
+            <ListItem divider>
+              <ListItemIcon>
+                <img src={comp1} alt="Logo" />
+              </ListItemIcon>
+              <ListItemIcon>
+                <img src={comp2} alt="Logo" />
+              </ListItemIcon>
+              <ListItemIcon>
+                <img src={comp3} alt="Logo" />
+              </ListItemIcon>
+            </ListItem>
+          </List>
+        </Collapse>
+
+        {/* Collapsing List Mapped by Libraries fetched by the API */}
+        {
+          libraries.map(
+            (library) => {
+              return (
+                <div key={library.id}>
+                  <ListItem onClick={(e, id = library.id) => handleCollapse(id)} button divider>
+                    <span className={classes.head}>{library.library_name}</span>
+                    {collapse[library.id] ? <ExpandLess /> : <ExpandMore />}
+                  </ListItem>
+                  <Collapse in={collapse[library.id]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding dense="true">
+                      <ListItem divider>
+                        <ListItemIcon>
+                          <img src={comp1} alt="Logo" />
+                        </ListItemIcon>
+                        <ListItemIcon>
+                          <img src={comp2} alt="Logo" />
+                        </ListItemIcon>
+                        <ListItemIcon>
+                          <img src={comp3} alt="Logo" />
+                        </ListItemIcon>
+                      </ListItem>
+                    </List>
+                  </Collapse>
+                </div>
+              )
+            }
+          )
+        }
+
+        <ListItem>
+          <div ref={props.compRef}></div>
+        </ListItem>
+      </List>
+    </>
+  )
+}
