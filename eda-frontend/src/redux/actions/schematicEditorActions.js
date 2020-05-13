ab9c7de13519ef03f4e5b5cf4bf572156022f0f5
Darsh <darshkpatel@gmail.com>
2020-05-13 12:22:30 +0530

    Handle Collapse , Reducer , Action , Error Handling for fetch

--- eda-frontend/src/redux/actions/schematicEditorActions.js
+++ eda-frontend/src/redux/actions/schematicEditorActions.js
@@ -2,13 +2,21 @@ import api from '../../utils/Api'
 import * as actions from './actions'
 
 export const fetchLibraries = () => (dispatch) => {
-  api.get('libraries/').then(
-    (res) => {
-      dispatch({
-        type: actions.FETCH_LIBRARIES,
-        payload: res.data
-      })
-      console.log('Fetched Libraries:', res)
-    }
-  )
+  api.get('libraries/')
+    .then(
+      (res) => {
+        dispatch({
+          type: actions.FETCH_LIBRARIES,
+          payload: res.data
+        })
+      }
+    )
+    .catch((err) => { console.error(err) })
+}
+
+export const toggleCollapse = (id) => (dispatch) => {
+  dispatch({
+    type: actions.TOGGLE_COLLAPSE,
+    payload: { id: id }
+  })
 }
--- eda-frontend/src/redux/reducers/schematicEditorReducer.js
+++ eda-frontend/src/redux/reducers/schematicEditorReducer.js
@@ -1,13 +1,31 @@
 import * as actions from '../actions/actions'
 
 const InitialState = {
-  libraries: []
+  libraries: [],
+  collapse: {}
 }
 
 export default function (state = InitialState, action) {
   switch (action.type) {
-    case actions.FETCH_LIBRARIES:
-      return { ...state, libraries: action.payload }
+    case actions.FETCH_LIBRARIES: {
+    // Add 'open' parameter to track open/close state of collapse
+      const collapse = {}
+      action.payload.forEach(element => {
+        collapse[element.id] = false
+      })
+      return { ...state, libraries: action.payload, collapse: collapse }
+    }
+
+    case actions.TOGGLE_COLLAPSE: {
+      const newCollapse = state.collapse
+      newCollapse[action.payload.id] = !newCollapse[action.payload.id]
+      console.log('Updating collapse', action.payload.id)
+      Object.assign(state.collapse, newCollapse)
+      return { ...state, collapse: { ...state.collapse, newCollapse } }
+    }
+
+    case 'inc':
+      return { ...state, cnt: state.cnt + 1 }
 
     default:
       return state
