383ff1e6b08184bd1e7babbbc52f092e41e245c9
dssudake <darshansudake555@gmail.com>
2020-05-17 12:45:54 +0530

    Modified Grid layout

--- eda-frontend/src/components/SchematicEditor/SchematicGrid.js
+++ eda-frontend/src/components/SchematicEditor/SchematicGrid.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-//import AddSideBarComponent from "./Helper/SideBar.js"
 
 import "./Helper/SchematicEditor.css";
 import LoadGrid from "./Helper/ComponentDrag.js";
@@ -10,11 +9,16 @@ export default class SchematicGrid extends Component {
   componentDidMount() {
     var container = ReactDOM.findDOMNode(this.refs.divGrid);
     var sidebar = ReactDOM.findDOMNode(this.props.compRef.current);
-    //var collapsebar = ReactDOM.findDOMNode(this.props.listref.current)
-    LoadGrid(container,sidebar);
+    LoadGrid(container, sidebar);
   }
 
   render() {
-    return <div className="grid-container" ref="divGrid" id="divGrid" />;
+    return (
+      <>
+        <center>
+          <div className="grid-container" ref="divGrid" id="divGrid" />
+        </center>
+      </>
+    );
   }
 }
