d88d1b85bc1334259de8307207b270f3feb80a7b
dssudake <darshansudake555@gmail.com>
2020-05-17 22:00:05 +0530

    Fixed Linting errors

--- eda-frontend/src/components/SchematicEditor/SchematicGrid.js
+++ eda-frontend/src/components/SchematicEditor/SchematicGrid.js
@@ -1,75 +1,28 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-import "./Helper/SchematicEditor.css";
-import LoadGrid from "./Helper/ComponentDrag.js";
+import './Helper/SchematicEditor.css'
+import LoadGrid from './Helper/ComponentDrag.js'
 
 export default class SchematicGrid extends Component {
-  
-  constructor(props){
-    super(props);
-    this.state = {
-      gridSize: "a4",
-      btClass: classNames('grid-container', `a4p`),
-      orientation: "p"
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleRadio = this.handleRadio.bind(this);
-
-
-
-  }
-  componentDidMount() {
-    var container = ReactDOM.findDOMNode(this.refs.divGrid);
-    var sidebar = ReactDOM.findDOMNode(this.props.compRef.current);
-    LoadGrid(container, sidebar);
-  }
-  handleChange(event) {
-    this.setState({gridSize: event.target.value, btClass:classNames('grid-container',`${event.target.value}${this.state.orientation}`)});
+  componentDidMount () {
+    var container = this.props.gridRef.current
+    var sidebar = this.props.compRef.current
+    LoadGrid(container, sidebar)
   }
 
-  handleRadio(event){
-    
-    this.setState({gridSize: this.state.gridSize, 
-      btClass:classNames('grid-container',`${this.state.gridSize}${event.target.value}`),
-      orientation: event.target.value
-    });
-    console.log(this.state)
+  render () {
+    return (
+      <>
+        <center>
+          <div className="grid-container A4-L" ref={this.props.gridRef} id="divGrid" />
+        </center>
+      </>
+    )
   }
-  
+}
 
-  render() {
-    return (<><div className={this.state.btClass} ref="divGrid" id="divGrid" />
-            
-              <div id="propertiesSidebar">
-              <span>select grid size: </span>
-              <select value={this.state.value} onChange={this.handleChange}>
-              <option value="a1">A1</option>
-              <option value="a2">A2</option>
-                <option value="a3">A3</option>
-            
-                
-                <option selected value="a4">A4</option>
-               
-               
-                <option value="a5">A5</option>
-              </select>
-             
-              
-                <div className="radio">
-                select orientation: 
-                <label>
-                  <input type="radio" value="p" onClick={this.handleRadio} checked={this.state.orientation === 'p'} />
-                  Portrait
-                </label>
-                <label>
-                  <input type="radio" value="l" onClick={this.handleRadio} checked={this.state.orientation === 'l'} />
-                  Landscape
-                </label>
-              </div>
-
-              
-              </div>
-    </>);
-  }
+SchematicGrid.propTypes = {
+  compRef: PropTypes.object.isRequired,
+  gridRef: PropTypes.object.isRequired
 }
--- eda-frontend/src/pages/SchematiEditor.js
+++ eda-frontend/src/pages/SchematiEditor.js
@@ -1,51 +1,52 @@
-import React from "react";
-import { CssBaseline, IconButton } from "@material-ui/core";
-import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react'
+import { CssBaseline, IconButton } from '@material-ui/core'
+import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined'
+import { makeStyles } from '@material-ui/core/styles'
 
-import Layout from "../components/Shared/Layout";
-import Header from "../components/SchematicEditor/Header";
-import ComponentSidebar from "../components/SchematicEditor/ComponentSidebar";
-import LayoutMain from "../components/Shared/LayoutMain";
-import SchematicGrid from "../components/SchematicEditor/SchematicGrid";
-import RightSidebar from "../components/SchematicEditor/RightSidebar";
-import PropertiesSidebar from "../components/SchematicEditor/PropertiesSidebar";
+import Layout from '../components/Shared/Layout'
+import Header from '../components/SchematicEditor/Header'
+import ComponentSidebar from '../components/SchematicEditor/ComponentSidebar'
+import LayoutMain from '../components/Shared/LayoutMain'
+import SchematicGrid from '../components/SchematicEditor/SchematicGrid'
+import RightSidebar from '../components/SchematicEditor/RightSidebar'
+import PropertiesSidebar from '../components/SchematicEditor/PropertiesSidebar'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    minHeight: "100vh",
+    display: 'flex',
+    minHeight: '100vh'
   },
   toolbar: {
-    minHeight: "80px",
+    minHeight: '80px'
   },
   menuButton: {
-    marginLeft: "auto",
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
-    },
-  },
-}));
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
+    }
+  }
+}))
 
-export default function SchematiEditor() {
-  const classes = useStyles();
+export default function SchematiEditor () {
+  const classes = useStyles()
 
-  const compRef = React.createRef();
+  const compRef = React.createRef()
+  const gridRef = React.createRef()
 
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = React.useState(false)
 
   const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    setMobileOpen(!mobileOpen)
+  }
 
   return (
     <div className={classes.root}>
       <CssBaseline />
 
       <Layout header={<Header />} resToolbar={<IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="end"
+        color='inherit'
+        aria-label='open drawer'
+        edge='end'
         onClick={handleDrawerToggle}
         className={classes.menuButton}
       >
@@ -54,12 +55,12 @@ export default function SchematiEditor() {
 
       <LayoutMain>
         <div className={classes.toolbar} />
-        <SchematicGrid compRef={compRef} />
+        <SchematicGrid gridRef={gridRef} compRef={compRef} />
       </LayoutMain>
 
       <RightSidebar mobileOpen={mobileOpen} mobileClose={handleDrawerToggle} >
-        <PropertiesSidebar />
+        <PropertiesSidebar gridRef={gridRef} />
       </RightSidebar>
     </div>
-  );
+  )
 }
