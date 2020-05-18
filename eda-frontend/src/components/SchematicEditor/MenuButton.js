28c789e6e666e2dcb453ff05f0ac2bf73112ec48
dssudake <darshansudake555@gmail.com>
2020-05-18 14:25:40 +0530

    Added ToolButtons to Toolbar

---
+++ eda-frontend/src/components/SchematicEditor/MenuButton.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {  IconButton, Menu, MenuItem, Fade } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = (theme) => ({
+  tools: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(0, 0.5)
+  }
+});
+
+class MenuButton extends React.Component {
+  state = {
+    anchorEl: null
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+    const Wrapper = this.props.iconType;
+    const listItems = this.props.items.map((link) =>
+      <MenuItem onClick={this.handleClose} >{link}</MenuItem>
+    );
+
+    return (
+      <>
+        <IconButton
+          aria-owns={open ? 'menu-toolbar' : null}
+          aria-haspopup="true"
+          onClick={this.handleMenu}
+          color="inherit"
+          className={classes.tools}
+          size="small"
+        >
+          {<Wrapper fontSize="small" />}
+        </IconButton>
+        <Menu
+          id="menu-toolbar"
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          open={open}
+          style={{ marginTop: "48px" }}
+          TransitionComponent={Fade}
+          onClose={this.handleClose}
+        >
+          {listItems}
+        </Menu>
+      </>
+    );
+  }
+}
+
+export default withStyles(styles, { withTheme: true })(MenuButton);
--- eda-frontend/src/components/SchematicEditor/SchematicToolbar.js
+++ eda-frontend/src/components/SchematicEditor/SchematicToolbar.js
@@ -2,15 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { IconButton } from '@material-ui/core'
 import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined'
+import FolderIcon from '@material-ui/icons/Folder'
+import EditIcon from '@material-ui/icons/Edit'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
+import UndoIcon from '@material-ui/icons/Undo'
+import RedoIcon from '@material-ui/icons/Redo'
+import ZoomInIcon from '@material-ui/icons/ZoomIn'
+import ZoomOutIcon from '@material-ui/icons/ZoomOut'
+import DeleteIcon from '@material-ui/icons/Delete'
 import { makeStyles } from '@material-ui/core/styles'
+
+import MenuButton from './MenuButton'
 
 const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginLeft: 'auto',
-    padding: theme.spacing(0.5),
+    padding: theme.spacing(1),
     [theme.breakpoints.up('sm')]: {
       display: 'none'
     }
+  },
+  tools: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(0, 0.5)
   }
 }))
 
@@ -19,6 +34,29 @@ export default function SchematicToolbar ({ mobileClose }) {
 
   return (
     <>
+      <MenuButton iconType={FolderIcon} items={['New', 'Open', 'Save', 'Print', 'Export']} />
+      <MenuButton iconType={EditIcon} items={['Cut', 'Copy', 'Paste']} />
+      <MenuButton iconType={PlayArrowIcon} items={['DC Simulation', 'DC Sweep', 'Time Domain Simulation', 'Frequency Domain Simulation']} />
+
+      <IconButton color="inherit" className={classes.tools} size="small">
+        <UndoIcon fontSize="small" />
+      </IconButton>
+      <IconButton color="inherit" className={classes.tools} size="small">
+        <RedoIcon fontSize="small" />
+      </IconButton>
+      <IconButton color="inherit" className={classes.tools} size="small">
+        <ZoomInIcon fontSize="small" />
+      </IconButton>
+      <IconButton color="inherit" className={classes.tools} size="small">
+        <ZoomOutIcon fontSize="small" />
+      </IconButton>
+      <IconButton color="inherit" className={classes.tools} size="small">
+        <DeleteIcon fontSize="small" />
+      </IconButton>
+      <IconButton color="inherit" className={classes.tools} size="small">
+        <HelpOutlineIcon fontSize="small" />
+      </IconButton>
+
       <IconButton
         color='inherit'
         aria-label='open drawer'
