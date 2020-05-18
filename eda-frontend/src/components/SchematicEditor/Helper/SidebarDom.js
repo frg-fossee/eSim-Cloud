47c64df13349fa77bf63132d1600e7b2879e5d23
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-05-18 11:30:34 +0530

    Linted DraggableDOM.js

--- eda-frontend/src/components/SchematicEditor/Helper/SidebarDom.js
+++ eda-frontend/src/components/SchematicEditor/Helper/SidebarDom.js
@@ -1,8 +1,8 @@
-//import mxGraphFactory from "mxgraph";
-//import comp1 from "../../../static/CircuitComp/4002_1_A.svg";
-//import getMetadataXML from "./xml_parser";
-import AddSideBarComponentDOMd from "./DraggableDOM.js"
-/*const {
+// import mxGraphFactory from "mxgraph";
+// import comp1 from "../../../static/CircuitComp/4002_1_A.svg";
+// import getMetadataXML from "./xml_parser";
+import AddSideBarComponentDOMd from './DraggableDOM.js'
+/* const {
   mxGraph,
   mxRubberband,
   mxClient,
@@ -10,19 +10,12 @@ import AddSideBarComponentDOMd from "./DraggableDOM.js"
   mxEvent,
   mxPoint,
   mxDragSource,
-} = new mxGraphFactory();*/
+} = new mxGraphFactory(); */
 
-
-
-export default function AddSideBarComponentDOM() {
- 
-   var a = document.getElementsByTagName('img')
-   
-Array.from(a).forEach(function (element) { 
+export default function AddSideBarComponentDOM () {
+  var a = document.getElementsByTagName('img')
+  Array.from(a).forEach(function (element) {
     AddSideBarComponentDOMd(element)
-    console.log(element) 
-  }); 
-
-
-
+    console.log(element)
+  })
 }
