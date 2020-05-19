c79c6d61f9901e8919a5c1e60c3aec742bb83852
rohit geddam <rohitgeddam@rohits-MacBook-Pro.local>
2020-05-19 15:46:00 +0530

    added simulation properties inputs

--- eda-frontend/src/components/SchematicEditor/PropertiesSidebar.js
+++ eda-frontend/src/components/SchematicEditor/PropertiesSidebar.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Hidden, List, ListItem, ListItemText, TextField, MenuItem } from '@material-ui/core'
+import { Hidden, List, ListItem, Typography,ListItemText, TextField, MenuItem } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import SimulationProperties from "./SimulationProperties"
 
 import './Helper/SchematicEditor.css'
 
@@ -103,6 +104,7 @@ function GridProperties ({ gridRef }) {
           ))}
         </TextField>
       </ListItem>
+      
     </>
   )
 }
@@ -130,6 +132,11 @@ export default function PropertiesSidebar ({ gridRef, outlineRef }) {
         </ListItem>
         <ListItem style={{ padding: '0px' }} divider>
           <div className="outline-container" ref={outlineRef} id="outlineContainer" />
+        </ListItem>
+        <ListItem>
+        
+        <SimulationProperties/>
+
         </ListItem>
       </List>
     </>
