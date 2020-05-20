41dde9a6cd9ffaa0ea65fa403563c391ad8715b0
Darsh <darshkpatel@gmail.com>
2020-05-20 17:57:45 +0530

    add seed libs to migrations

--- esim-cloud-backend/migrations.sh
+++ esim-cloud-backend/migrations.sh
@@ -4,3 +4,4 @@ python manage.py migrate simulationAPI --database="mongodb"
 python manage.py makemigrations libAPI
 python manage.py migrate libAPI
 python manage.py migrate
+python manage.py seed_libs --location kicad-symbols/
