10c9961b79c4bfc5be4fbd4ee097030d837802eb
Navonil Das <navneeladas@gmail.com>
2020-05-21 13:12:33 +0530

    Compile Sketch API View

--- esim-cloud-backend/arduinoAPI/views.py
+++ esim-cloud-backend/arduinoAPI/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from arduinoAPI.tasks import compile_sketch_task
+import uuid
 
-# Create your views here.
+
+class CompileSketch(APIView):
+    def post(self, request):
+        """
+        Compile list of Arduino Sketch File
+
+        body: {<Arduino ID>:<source code>}
+        example: { "1":"void setup(){}void loop(){}"}
+        """
+        # Create Task ID (Used for getting Response)
+        task_id = uuid.uuid4()
+        # Queue Task
+        task = compile_sketch_task.apply_async(
+            kwargs={
+                'data': request.data,
+                'task_id': task_id
+            }, task_id=str(task_id))
+        # Return Status
+        return Response({
+            'state': task.state,
+            'uuid': str(task_id)
+        })
