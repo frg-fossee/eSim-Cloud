5ac4b30790f663388afe73f7f4340f6793723732
Darsh <darshkpatel@gmail.com>
2020-05-23 01:04:50 +0530

    updated seeding function

--- esim-cloud-backend/migrations.sh
+++ esim-cloud-backend/migrations.sh
@@ -4,4 +4,6 @@ python manage.py migrate simulationAPI --database="mongodb"
 python manage.py makemigrations libAPI
 python manage.py migrate libAPI
 python manage.py migrate
+rm -r kicad-symbols/symbol_svgs/
+python manage.py seed_libs --clear
 python manage.py seed_libs --location kicad-symbols/
