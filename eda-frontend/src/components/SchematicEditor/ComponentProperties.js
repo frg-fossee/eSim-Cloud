6f3ab3c8caae2a350627ba45ab35b1dba3ddb71b
dssudake <darshansudake555@gmail.com>
2020-05-26 20:03:04 +0530

    List component properties on right sidebar

--- eda-frontend/src/components/SchematicEditor/ComponentProperties.js
+++ eda-frontend/src/components/SchematicEditor/ComponentProperties.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { ListItem, ListItemText, Button } from '@material-ui/core'
+import { ListItem, ListItemText, Button, TextField } from '@material-ui/core'
 
 export default function ComponentProperties () {
   const properties = useSelector(state => state.componentPropertiesReducer.compProperties)
@@ -13,6 +13,13 @@ export default function ComponentProperties () {
       <ListItem>
         <ListItemText primary={properties.FNAME} />
       </ListItem>
+      {
+        Object.keys(properties).map((keyName, i) => (
+          <ListItem key={i}>
+            <TextField id={keyName} label={keyName} value={properties[keyName]} size='small' variant="outlined" />
+          </ListItem>
+        ))
+      }
       <ListItem>
         <Button size='small' variant="contained" color="primary">ADD</Button>
       </ListItem>
