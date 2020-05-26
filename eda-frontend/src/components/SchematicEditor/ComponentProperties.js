71ca3abd0aeb6aeb7592381624fe38be5b0e32cd
rohit geddam <rohitgeddam2018@gmail.com>
2020-05-26 20:25:50 +0530

    populating input fields

--- eda-frontend/src/components/SchematicEditor/ComponentProperties.js
+++ eda-frontend/src/components/SchematicEditor/ComponentProperties.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { ListItem, ListItemText, Button, TextField } from '@material-ui/core'
 
 export default function ComponentProperties () {
   const properties = useSelector(state => state.componentPropertiesReducer.compProperties)
 
+  const [val, setVal] = useState(properties)
+
+  const getInputValues = (evt) => {
+    console.log(evt.target.value)
+    const value = evt.target.value
+
+    setVal({
+      ...val,
+      [evt.target.id]: value
+    })
+  }
   return (
     <div>
       <ListItem>
@@ -15,14 +26,17 @@ export default function ComponentProperties () {
       </ListItem>
       {
         Object.keys(properties).map((keyName, i) => (
+
           <ListItem key={i}>
-            <TextField id={keyName} label={keyName} value={properties[keyName]} size='small' variant="outlined" />
+            <TextField id={keyName} label={keyName} value={val[keyName]} size='small' variant="outlined" onChange={getInputValues} />
           </ListItem>
         ))
       }
+
       <ListItem>
         <Button size='small' variant="contained" color="primary">ADD</Button>
       </ListItem>
+
     </div>
   )
 }
