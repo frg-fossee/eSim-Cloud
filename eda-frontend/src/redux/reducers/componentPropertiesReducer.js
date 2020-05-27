f731c0fbeb5cacc28017bf072f5e9949a5aab2f7
rohit geddam <rohitgeddam2018@gmail.com>
2020-05-27 19:24:01 +0530

    fixed expression and model textarea not saving the state

--- eda-frontend/src/redux/reducers/componentPropertiesReducer.js
+++ eda-frontend/src/redux/reducers/componentPropertiesReducer.js
@@ -18,6 +18,7 @@ export default function (state = InitialState, action) {
     }
 
     case actions.SET_COMP_PROPERTIES: {
+      console.log(action.payload.compProperties)
       return {
         ...state,
         id: action.payload.id,
