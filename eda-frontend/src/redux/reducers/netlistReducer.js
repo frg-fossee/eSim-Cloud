ad51f3ae779a71f95cd7f0b4857457446ca1ae11
rohit geddam <rohitgeddam2018@gmail.com>
2020-05-27 22:10:44 +0530

    add reducers for different parts of netlist

--- eda-frontend/src/redux/reducers/netlistReducer.js
+++ eda-frontend/src/redux/reducers/netlistReducer.js
@@ -1,12 +1,44 @@
 import * as actions from '../actions/netlistActions'
 
-const initialState = ''
+const initialState = {
+  title: '',
+  model: '',
+  netlist: '',
+  controlLine: '',
+  controlBlock: ''
+}
 
 export default function (state = initialState, action) {
   switch (action.type) {
-    case actions.setNetlist: {
+    case actions.SET_NETLIST: {
       return {
+        ...state,
         netlist: action.payload.netlist
+      }
+    }
+    case actions.SET_TITLE: {
+      return {
+        ...state,
+        title: action.payload.title
+      }
+    }
+
+    case actions.SET_MODEL: {
+      return {
+        ...state,
+        model: action.payload.model
+      }
+    }
+    case actions.SET_CONTROL_LINE: {
+      return {
+        ...state,
+        controlLine: action.payload.controlLine
+      }
+    }
+    case actions.SET_CONTROL_BLOCK: {
+      return {
+        ...state,
+        controlLine: action.payload.controlLine
       }
     }
     default:
