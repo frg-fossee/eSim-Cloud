511c6085faca36bab757e89f1d12a5fe49303102
rohit geddam <rohitgeddam2018@gmail.com>
2020-05-28 23:30:27 +0530

    sign up form functionality

--- eda-frontend/src/pages/signUp.js
+++ eda-frontend/src/pages/signUp.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   Container,
   Grid,
@@ -14,7 +14,7 @@ import {
 import { makeStyles } from '@material-ui/core/styles'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import { Link as RouterLink } from 'react-router-dom'
-
+import axios from "axios"
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(27),
@@ -39,6 +39,63 @@ const useStyles = makeStyles((theme) => ({
 export default function SignUp () {
   const classes = useStyles()
 
+    const [userCredentials,setUserCredentials] = useState({email:"",username:"",password:"",password2:""})
+    const [error,setError] = useState({eemail:false,eusername:false,epassword:false,epassword2:false})
+
+const changeHandler = (e) => {
+    setUserCredentials({...userCredentials,[e.target.name]: e.target.value })
+}
+
+const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(userCredentials)
+    console.log("HELLO")
+
+}
+
+
+const validate = () => {
+    let isError = false;
+    let errors = {}
+
+    if (userCredentials.username.length < 5){
+        isError = true;
+        errors["eusername"] = true
+    }else{
+        errors["eusername"] = false
+    }
+
+    if(isError){
+        setError(Object.assign(error,errors))
+    }
+    console.log(error)
+
+    return isError
+}
+
+const submit = () => {
+    console.log(userCredentials)
+
+
+    const err = validate();
+
+    if(!err){
+        const cred = userCredentials
+        delete cred.password2
+
+        axios.post('http://localhost/api/auth/users/', cred)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }else{
+        console.log("error")
+    }
+
+}
+
   useEffect(() => {
     document.title = 'Sign Up - EDA '
   })
@@ -54,28 +111,35 @@ export default function SignUp () {
           Register | Sign Up
         </Typography>
 
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={submitHandler}>
         <TextField
             variant="outlined"
             margin="normal"
             required
             fullWidth
             id="email"
-            label="email"
+            label="Email"
             name="email"
             autoComplete="email"
+            error = {error.eemail}
+            helperText={error.eemail ? "invalid email": ""}
             autoFocus
+            value={userCredentials.email}
+            onChange={changeHandler}
           />
           <TextField
             variant="outlined"
             margin="normal"
             required
             fullWidth
-            id="Username"
+            id="username"
             label="Username"
-            name="Username"
+            name="username"
+            error={error.eusername}
+            helperText={error.eusername ? "invalid username": ""}
             autoComplete="Username"
-            autoFocus
+            value={userCredentials.username}
+            onChange={changeHandler}
           />
           <TextField
             variant="outlined"
@@ -86,6 +150,9 @@ export default function SignUp () {
             label="Password"
             type="password"
             id="password"
+            error={error.epassword}
+            value={userCredentials.password}
+            onChange={changeHandler}
             autoComplete="current-password"
           />
           <TextField
@@ -93,23 +160,27 @@ export default function SignUp () {
             margin="normal"
             required
             fullWidth
-            name="Confirm password"
+            name="password2"
             label="Confirm password"
             type="password"
             id="Confirm_password"
+            error={error.epassword2}
+            value={userCredentials.password2}
+            onChange={changeHandler}
             autoComplete="current-password"
           />
-          <FormControlLabel
+          {/* <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="I accept the Terms of Use & Privacy Policy"
-          />
+          /> */}
           <Button
             component={RouterLink}
-            to="/dashboard"
+            // to="/dashboard"
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
+            // onClick={submit}
             className={classes.submit}
           >
             Login
@@ -117,7 +188,7 @@ export default function SignUp () {
 
         </form>
       </Card>
-      <Button
+      {/* <Button
         component={RouterLink}
         to="/"
         type="Sign Up"
@@ -126,7 +197,7 @@ export default function SignUp () {
         className={classes.submit}
       >
         Back to home
-      </Button>
+      </Button> */}
     </Container>
   )
 }
