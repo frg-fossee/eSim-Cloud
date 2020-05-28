2b746c510dd7b029b553fe80f6a873b5e4910cd9
Navonil Das <navneeladas@gmail.com>
2020-05-28 16:24:38 +0530

    Save Data For Led

--- ArduinoFrontend/src/app/Libs/outputs/Led.ts
+++ ArduinoFrontend/src/app/Libs/outputs/Led.ts
@@ -4,13 +4,17 @@ import { Point } from '../Point';
 declare var window;
 
 export class LED extends CircuitElement {
-  static pointHalf = 4;
   static colors: string[] = []; // Color of LED
   static glowColors: string[] = [];
   static colorNames: string[] = [];
   selectedIndex = 0;
   constructor(public canvas: any, x: number, y: number) {
     super('LED', x, y, 'LED.json', canvas);
+  }
+  SaveData() {
+    return {
+      color: this.selectedIndex
+    };
   }
   init() {
     if (LED.glowColors.length === 0) {
@@ -88,7 +92,6 @@ export class LED extends CircuitElement {
 }
 
 export class RGBLED extends CircuitElement {
-  static pointHalf = 4;
   constructor(public canvas: any, x: number, y: number) {
     super('RGBLED', x, y, 'RGBLED.json', canvas);
   }
