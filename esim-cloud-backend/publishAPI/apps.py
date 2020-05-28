a90b91c70ae40f0b25a5376e7d42b2fbc24c5974
Darsh <darshkpatel@gmail.com>
2020-05-28 14:00:42 +0530

    post migrate signal for groups

--- esim-cloud-backend/publishAPI/apps.py
+++ esim-cloud-backend/publishAPI/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class PublishapiConfig(AppConfig):
     name = 'publishAPI'
+
+    def ready(self):
+        from .signals import populate_models
+        post_migrate.connect(populate_models, sender=self)
