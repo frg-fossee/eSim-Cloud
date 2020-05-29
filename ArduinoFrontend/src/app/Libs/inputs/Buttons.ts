704202a0e02edb7365744d57ba66bf8f60eb85d6
Navonil Das <navneeladas@gmail.com>
2020-05-29 17:47:09 +0530

    Simulation Logic For Push Button

--- ArduinoFrontend/src/app/Libs/inputs/Buttons.ts
+++ ArduinoFrontend/src/app/Libs/inputs/Buttons.ts
@@ -4,8 +4,29 @@ import { Point } from '../Point';
 declare var Raphael;
 
 export class PushButton extends CircuitElement {
+  pinNamedMap: any = {};
   constructor(public canvas: any, x: number, y: number) {
     super('PushButton', x, y, 'PushButton.json', canvas);
+  }
+  init() {
+    for (const node of this.nodes) {
+      this.pinNamedMap[node.label] = node;
+    }
+    this.pinNamedMap['Terminal 1a'].addValueListener((v) => {
+      this.pinNamedMap['Terminal 1b'].setValue(v, null);
+    });
+    this.pinNamedMap['Terminal 1b'].addValueListener((v) => {
+      this.pinNamedMap['Terminal 1a'].setValue(v, null);
+    });
+    this.pinNamedMap['Terminal 2a'].addValueListener((v) => {
+      this.pinNamedMap['Terminal 2b'].setValue(v, null);
+    });
+    this.pinNamedMap['Terminal 2b'].addValueListener((v) => {
+      this.pinNamedMap['Terminal 2a'].setValue(v, null);
+    });
+  }
+  logic(v) {
+
   }
   load(data: any): void {
   }
@@ -19,8 +40,41 @@ export class PushButton extends CircuitElement {
     };
   }
   initSimulation(): void {
+    // console.log(this.pinNamedMap[''])
+    this.elements.unmousedown();
+    let iniValue = -1;
+    let by = -1;
+    this.elements[9].mousedown(() => {
+      const val = this.pinNamedMap['Terminal 1a'].value;
+      if (val > 0) {
+        by = 0;
+        iniValue = this.pinNamedMap['Terminal 2a'].value;
+        this.pinNamedMap['Terminal 2a'].setValue(val, null);
+        this.pinNamedMap['Terminal 2b'].setValue(val, null);
+      } else {
+        by = 1;
+        iniValue = this.pinNamedMap['Terminal 1a'].value;
+        this.pinNamedMap['Terminal 1a'].setValue(val, null);
+        this.pinNamedMap['Terminal 1b'].setValue(val, null);
+      }
+    });
+    this.elements[9].mouseup(() => this.MouseUp(by, iniValue));
+    this.elements[9].mouseout(() => this.MouseUp(by, iniValue));
+  }
+  MouseUp(by: number, iniValue: number) {
+    if (by === 0) {
+      this.pinNamedMap['Terminal 2a'].setValue(iniValue, null);
+      this.pinNamedMap['Terminal 2b'].setValue(iniValue, null);
+    } else {
+      this.pinNamedMap['Terminal 1a'].setValue(iniValue, null);
+      this.pinNamedMap['Terminal 1b'].setValue(iniValue, null);
+    }
   }
   closeSimulation(): void {
+    this.elements.unmousedown();
+    this.elements.unmouseup();
+    this.elements.unmouseout();
+    this.setClickListener(null);
   }
   simulate(): void {
   }
