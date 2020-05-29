65da6a17cb9067af44e0caee1a578044add2d40d
Darsh <darshkpatel@gmail.com>
2020-05-29 11:22:21 +0530

    fix linter error

--- esim-cloud-backend/publishAPI/urls.py
+++ esim-cloud-backend/publishAPI/urls.py
@@ -3,7 +3,6 @@
 esimCloud URL Configuration
 
 """
-from django.urls import path
 from django.conf.urls import url, include
 from publishAPI import views as publishAPI_views
 from rest_framework import routers
