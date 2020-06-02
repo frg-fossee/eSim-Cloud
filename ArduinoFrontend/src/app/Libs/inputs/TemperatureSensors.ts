dbd6201cad260aa4679d2377849bccf266b60ee7
Navonil Das <navneeladas@gmail.com>
2020-06-02 11:06:27 +0530

    Slider on Temperature Sensor(Ongoing)

--- ArduinoFrontend/src/app/Libs/inputs/TemperatureSensors.ts
+++ ArduinoFrontend/src/app/Libs/inputs/TemperatureSensors.ts
@@ -1,9 +1,13 @@
 import { CircuitElement } from '../CircuitElement';
 import { Point } from '../Point';
+import { Slider } from './Slider';
 
 export class LM35 extends CircuitElement {
   constructor(public canvas: any, x: number, y: number) {
     super('LM35', x, y, 'LM35.json', canvas);
+  }
+  init() {
+    const y = new Slider(this.canvas, this.x, this.y - 10);
   }
   properties(): { keyName: string; id: number; body: HTMLElement; title: string; } {
     const body = document.createElement('div');
