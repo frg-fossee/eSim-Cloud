94b85e150c29785c8abdec6fe0c24398aafb0508
Navonil Das <navneeladas@gmail.com>
2020-06-07 10:26:15 +0530

    Removed LM35 and implemented TMP36

--- ArduinoFrontend/src/app/Libs/inputs/TemperatureSensors.ts
+++ ArduinoFrontend/src/app/Libs/inputs/TemperatureSensors.ts
@@ -2,12 +2,14 @@ import { CircuitElement } from '../CircuitElement';
 import { Point } from '../Point';
 import { Slider } from './Slider';
 
-export class LM35 extends CircuitElement {
+export class TMP36 extends CircuitElement {
+  slide: Slider;
   constructor(public canvas: any, x: number, y: number) {
-    super('LM35', x, y, 'LM35.json', canvas);
+    super('TMP36', x, y, 'TMP36.json', canvas);
   }
   init() {
-    const y = new Slider(this.canvas, this.x, this.y - 10);
+    console.log(this.nodes[0].label);
+    console.log(this.nodes[2].label);
   }
   properties(): { keyName: string; id: number; body: HTMLElement; title: string; } {
     const body = document.createElement('div');
@@ -18,9 +20,32 @@ export class LM35 extends CircuitElement {
       title: 'Temperatur Sensor'
     };
   }
+  setValue(val: number) {
+    if (
+      this.nodes[0].connectedTo && this.nodes[0].value >= 4.9 &&
+      this.nodes[2].connectedTo
+    ) {
+      this.nodes[1].setValue(val, null);
+    } else {
+      window['showToast']('Please Connect Wires Properly');
+    }
+  }
   initSimulation(): void {
+    this.slide = new Slider(this.canvas, this.x + this.tx, this.y + this.ty - 10);
+    this.slide.minv = 0;
+    this.slide.maxv = 1;
+    this.slide.setValueChangeListener((v) => {
+      const tmp = v * 165 + -40; // Temperature
+      // console.log([tmp, (tmp + 50) / 100]);
+      // this.nodes[1].setValue((tmp + 50) / 100, null);
+      this.setValue((tmp + 50) / 100);
+    });
+    this.setValue(0.925);
   }
   closeSimulation(): void {
+    this.slide.remove();
+    delete this.slide;
+    this.slide = null;
   }
   simulate(): void {
   }
