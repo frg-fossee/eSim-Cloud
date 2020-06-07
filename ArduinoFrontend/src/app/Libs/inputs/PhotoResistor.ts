f14b1973cc6ee9df66963f8321c6522d3044485b
Navonil Das <navneeladas@gmail.com>
2020-06-07 12:36:03 +0530

    Photoresistor Slider

--- ArduinoFrontend/src/app/Libs/inputs/PhotoResistor.ts
+++ ArduinoFrontend/src/app/Libs/inputs/PhotoResistor.ts
@@ -1,7 +1,9 @@
 import { CircuitElement } from '../CircuitElement';
-import { Point } from '../Point';
+import { Slider } from './Slider';
 
 export class PhotoResistor extends CircuitElement {
+  slide: Slider;
+  valueText: any;
   constructor(public canvas: any, x: number, y: number) {
     super('PhotoResistor', x, y, 'PhotoResistor.json', canvas);
   }
@@ -14,9 +16,39 @@ export class PhotoResistor extends CircuitElement {
       title: 'Photo Resistor'
     };
   }
+  getValue(r: number) {
+    let tmp = r;
+    let suffix = 'Ω';
+    if (tmp > 1000) {
+      tmp /= 1000;
+      suffix = 'K' + suffix;
+    }
+    return `${Math.round((tmp) * 100) / 100}${suffix}`;
+  }
   initSimulation(): void {
+    this.valueText = this.canvas.text(this.x + this.tx + 120, this.y + this.ty - 40, '90.25KΩ');
+    this.valueText.attr({
+      'font-size': 15
+    });
+    this.slide = new Slider(this.canvas, this.x + this.tx, this.y + this.ty - 10);
+    this.slide.setGradient('#69644b', '#ffd500');
+    this.slide.setValueChangeListener((v) => {
+      const tmp = (1 - v) * 179500 + 500; // Temperature
+      // console.log(tmp);
+      // console.log([tmp, (tmp + 50) / 100]);
+      // this.nodes[1].setValue((tmp + 50) / 100, null);
+      this.valueText.attr({
+        text: this.getValue(tmp)
+      });
+    });
   }
   closeSimulation(): void {
+    this.valueText.remove();
+    this.slide.remove();
+    delete this.slide;
+    delete this.valueText;
+    this.slide = null;
+    this.valueText = null;
   }
   simulate(): void {
   }
