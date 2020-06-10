270cfd55d0a48e7c07b4ef399c008fff5b8ee6ee
dssudake <darshansudake555@gmail.com>
2020-06-10 15:28:30 +0530

    Added gallery page for sample schematics

---
+++ eda-frontend/src/pages/Gallery.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import {
+  Card,
+  Grid,
+  Button,
+  Typography,
+  CardActions,
+  CardContent,
+  Container,
+  CssBaseline
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link as RouterLink } from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+  mainHead: {
+    width: '100%',
+    backgroundColor: '#404040',
+    color: '#fff'
+  },
+  title: {
+    fontSize: 16,
+    color: '#80ff80'
+  },
+  header: {
+    padding: theme.spacing(5, 0, 6, 0)
+  },
+  root: {
+    display: 'flex',
+    minHeight: '100vh',
+    backgroundColor: '#f4f6f8'
+  }
+}))
+
+function MainCard () {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.mainHead}>
+      <CardContent>
+        <Typography className={classes.title} gutterBottom>
+          sample schematics are listed below...
+        </Typography>
+        <Typography variant="h4" gutterBottom>
+          eSim Gallery
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          target="_blank"
+          component={RouterLink}
+          to="/editor"
+          size="small"
+          color="primary"
+        >
+          Create New
+        </Button>
+        <Button size="small" color="secondary">
+          Load More
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default function Gallery () {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container maxWidth="lg" className={classes.header}>
+        <Grid
+          container
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+          alignContent="center"
+          spacing={3}
+        >
+          {/* eSim Gallery Header */}
+          <Grid item xs={12}>
+            <MainCard />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  )
+}
