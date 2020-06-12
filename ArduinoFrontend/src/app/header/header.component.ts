1b8be19fbfdbc2c30c5dc4defd04b7bef3b5a5b7
Navonil Das <navneeladas@gmail.com>
2020-06-12 10:15:32 +0530

    Header Logout and Username

--- ArduinoFrontend/src/app/header/header.component.ts
+++ ArduinoFrontend/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Login } from '../Libs/Login';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-header',
@@ -8,17 +9,30 @@ import { Login } from '../Libs/Login';
 })
 export class HeaderComponent implements OnInit {
   token: string;
+  username: string;
   @Input() title: string;
   @Input() color: string;
   @Input() front = false;
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
     this.token = Login.getToken();
+    if (this.token) {
+      this.api.userInfo(this.token).subscribe((v) => {
+        this.username = v.username;
+      }, (err) => {
+        // console.log(err.status)
+        // console.log(err);
+        if (err.status === 401) {
+          Login.logout();
+        }
+      });
+    }
   }
   Login() {
-
+    Login.redirectLogin(this.front);
   }
   Logout() {
+    Login.logout();
   }
 }
