b9c290706781cf95ea5a0be6f57915e1e0de60d3
Navonil Das <navneeladas@gmail.com>
2020-06-16 09:49:18 +0530

    Fixed issue with Gas Sensor

--- ArduinoFrontend/src/app/Libs/inputs/Collision.ts
+++ ArduinoFrontend/src/app/Libs/inputs/Collision.ts
@@ -87,6 +87,6 @@ export class Collision {
    * @param b coordinate b
    */
   static EuclideanDistance(a: Vector, b: Vector) {
-    return ((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y));
+    return Math.sqrt(((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y)));
   }
 }
