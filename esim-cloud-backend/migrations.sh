d31776c6fb212a2f9ba47440a7b72d61cfa5b024
Darsh <darshkpatel@gmail.com>
2020-06-27 12:03:16 +0530

    remove mongodb

--- esim-cloud-backend/migrations.sh
+++ esim-cloud-backend/migrations.sh
@@ -1,6 +1,6 @@
 python manage.py makemigrations
 python manage.py makemigrations simulationAPI
-python manage.py migrate simulationAPI --database="mongodb"
+python manage.py migrate simulationAPI
 python manage.py makemigrations libAPI
 python manage.py migrate libAPI
 python manage.py makemigrations saveAPI
