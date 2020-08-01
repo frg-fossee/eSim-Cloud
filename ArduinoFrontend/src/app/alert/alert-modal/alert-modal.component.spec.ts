629f75012fe133fc88d833263a1cbafc404e5a69
gupta-arpit <gupta.arpit985@gmail.com>
2020-08-01 11:41:31 +0530

    Squashed commit of the following:
    commit b6bf9d958f440889c4572633d63ef95f9d78b857
    Author: gupta-arpit <gupta.arpit985@gmail.com>
    Date:   Sat Aug 1 01:26:09 2020 +0530
    
        doc typo
    
    commit 89ec49afddf28b97465bfc5cc375e731a1fd545b
    Author: gupta-arpit <gupta.arpit985@gmail.com>
    Date:   Sat Aug 1 01:20:41 2020 +0530
    
        lint issues
    
    commit ea3e277b4b2810489862e56d5874f43de358bfa0
    Author: gupta-arpit <gupta.arpit985@gmail.com>
    Date:   Sat Aug 1 01:16:13 2020 +0530
    
        replacing alert service for more components
    
    commit 2ff17acd74a332de3fd9665e8c4598fea77bf306
    Author: gupta-arpit <gupta.arpit985@gmail.com>
    Date:   Sat Aug 1 01:02:29 2020 +0530
    
        Adding alert service
    
    commit fb33c306bb9ba37a102a26bb700e28464d1929f9
    Author: gupta-arpit <gupta.arpit985@gmail.com>
    Date:   Sat Aug 1 00:56:44 2020 +0530
    
        Replacing browser alert by material alert modal
    
    commit 618ec052abfe86536efd9da54dfc4fa1993e78d0
    Author: Arpit Gupta <arpit@falconx.io>
    Date:   Sat Aug 1 00:45:24 2020 +0530
    
        Revert "Replacing browser alert by material alert modal"
    
        This reverts commit 8873e9c85e618b7ddbe41835cff00189d2117b15.
    
    commit 1bfd6f4d1bea223d97eb0477e94df4c7d6ee7a99
    Author: Arpit Gupta <arpit@falconx.io>
    Date:   Sat Aug 1 00:45:17 2020 +0530
    
        Revert "lint fix"
    
        This reverts commit c02a15255d81bf87381dcb2694ab36a4080a0b35.
    
    commit c02a15255d81bf87381dcb2694ab36a4080a0b35
    Author: Arpit Gupta <arpit@falconx.io>
    Date:   Sat Aug 1 00:19:32 2020 +0530
    
        lint fix
    
    commit 8873e9c85e618b7ddbe41835cff00189d2117b15
    Author: Arpit Gupta <arpit@falconx.io>
    Date:   Sat Aug 1 00:16:09 2020 +0530
    
        Replacing browser alert by material alert modal

---
+++ ArduinoFrontend/src/app/alert/alert-modal/alert-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AlertModalComponent } from './alert-modal.component';
+
+describe('AlertModalComponent', () => {
+  let component: AlertModalComponent;
+  let fixture: ComponentFixture<AlertModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AlertModalComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AlertModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
