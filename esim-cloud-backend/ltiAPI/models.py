db5b374e926c5c692ef7aeb19135e669b55d789b
Akshat Sharma <akshatsharma2602@gmail.com>
2021-05-28 19:11:33 +0530

    Added submission route and submission models so that students can submit using the submit button

--- esim-cloud-backend/ltiAPI/models.py
+++ esim-cloud-backend/ltiAPI/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from saveAPI.models import StateSave
+from django.contrib.auth import get_user_model
+import uuid
 
 
 # Create your models here.
@@ -8,3 +10,24 @@ class lticonsumer(models.Model):
     secret_key = models.CharField(max_length=50, null=False)
     save_id = models.ForeignKey(to=StateSave, on_delete=models.CASCADE)
     score = models.FloatField()
+
+
+class ltiSession(models.Model):
+    user_id = models.CharField(max_length=100)
+    lis_result_sourcedid = models.CharField(max_length=100, null=True)
+    lis_outcome_service_url = models.CharField(max_length=100)
+    oauth_nonce = models.CharField(max_length=100)
+    oauth_timestamp = models.CharField(max_length=100)
+    oauth_consumer_key = models.CharField(max_length=100)
+    oauth_signature_method = models.CharField(max_length=100)
+    oauth_version = models.CharField(max_length=100)
+    oauth_signature = models.CharField(max_length=100)
+
+
+class Submission(models.Model):
+    project = models.ForeignKey(to=lticonsumer, on_delete=models.CASCADE)
+    student = models.ForeignKey(to=get_user_model(), blank=True, null=True, on_delete=models.CASCADE)
+    score = models.FloatField()
+    ltisession = models.ForeignKey(to=ltiSession, on_delete=models.CASCADE, null=True)
+    schematic = models.ForeignKey(to=StateSave, on_delete=models.CASCADE)
+    lms_success = models.BooleanField(null=True)
--- esim-cloud-backend/ltiAPI/serializers.py
+++ esim-cloud-backend/ltiAPI/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import lticonsumer
+from .models import lticonsumer, ltiSession, Submission
 
 
 class consumerSerializer(serializers.ModelSerializer):
@@ -18,3 +18,23 @@ class consumerResponseSerializer(serializers.Serializer):
     consumer_key = serializers.CharField(max_length=50)
     secret_key = serializers.CharField(max_length=50)
     score = serializers.FloatField()
+
+
+class SessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ltiSession
+        fields = ["id", "user_id", "oauth_nonce"]
+
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    ltisession = SessionSerializer(many=False)
+
+    class Meta:
+        model = Submission
+        fields = ["ltisession", "schematic"]
+
+
+class GetSubmissionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Submission
+        fields = ["schematic", "student", "project", "score", "lms_success", "ltisession"]
