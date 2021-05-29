408bb5def25bc3f9d02e407b3b214dcaa28f03af
Mili Mehta <63444165+MMM-9@users.noreply.github.com>
2021-05-29 22:22:21 +0530

    Add files via upload

---
+++ Changes Made by Me/L293D.ts
@@ -0,0 +1,209 @@
+import { CircuitElement } from '../CircuitElement';
+import { Point } from '../Point';
+import { ArduinoUno } from './Arduino';
+
+/**
+ * MotorDriver L298N class
+ */
+export class L293D extends CircuitElement {
+  /**
+   * Pin Name mapped to Pins
+   */
+  pinNamedMap: any = {};
+  /**
+   * Speed of Motor A in range of 0 to 5.
+   */
+  speedA = 5;
+  /**
+   * Speed of Motor B in range of 0 to 5
+   */
+  speedB = 5;
+  /**
+   * Previous values of the pins.
+   */
+  prevValues: any = {
+    IN1: -1,
+    IN2: -1,
+    IN3: -1,
+    IN4: -1
+  };
+
+  /**
+   * MotorDriver L298N constructor
+   * @param canvas Raphael Canvas (Paper)
+   * @param x  position x
+   * @param y  position y
+   */
+  constructor(public canvas: any, x: number, y: number) {
+    super('L293D', x, y, 'L293D.json', canvas);
+  }
+  /**
+   * Initialize motor class.
+   */
+  init() {
+    for (const node of this.nodes) {
+      this.pinNamedMap[node.label] = node;
+    }
+    this.pinNamedMap['V IN'].addValueListener(v => {
+      this.pinNamedMap['GND'].setValue(v, this.pinNamedMap['GND']);
+      if (v >= 5) {
+        this.pinNamedMap['5V IN'].setValue(5, this.pinNamedMap['5V IN']);
+      }
+      this.update();
+    });
+
+    this.pinNamedMap['IN1'].addValueListener(v => {
+      if (v !== this.prevValues.IN1) {
+        this.prevValues.IN1 = v;
+        this.update();
+      }
+    });
+    this.pinNamedMap['IN2'].addValueListener(v => {
+      if (v !== this.prevValues.IN2) {
+        this.prevValues.IN2 = v;
+        this.update();
+      }
+    });
+    this.pinNamedMap['IN3'].addValueListener(v => {
+      if (v !== this.prevValues.IN3) {
+        this.prevValues.IN3 = v;
+        this.update();
+      }
+    });
+    this.pinNamedMap['IN4'].addValueListener(v => {
+      if (v !== this.prevValues.IN4) {
+        this.prevValues.IN4 = v;
+        this.update();
+      }
+    });
+  }
+  /**
+   * Simulation Logic For L298N motor driver
+   */
+  update() {
+    setTimeout(() => {
+      if (this.pinNamedMap['IN1'].value > 0 && this.pinNamedMap['IN2'].value > 0) {
+        window['showToast']('Both IN1 and IN2 Pins are High!');
+        return;
+      }
+
+      if (this.pinNamedMap['IN3'].value > 0 && this.pinNamedMap['IN4'].value > 0) {
+        window['showToast']('Both IN3 and IN4 Pins are High!');
+        return;
+      }
+    }, 100);
+
+    if (this.pinNamedMap['IN1'].value > 0) {
+      this.pinNamedMap['Terminal 2'].setValue(
+        this.pinNamedMap['V IN'].value * (this.speedA / 5),
+        this.pinNamedMap['Terminal 2']
+      );
+    } else if (this.pinNamedMap['IN2'].value > 0) {
+      this.pinNamedMap['Terminal 1'].setValue(
+        this.pinNamedMap['V IN'].value * (this.speedA / 5),
+        this.pinNamedMap['Terminal 1']
+      );
+    } else {
+      this.pinNamedMap['Terminal 1'].setValue(
+        0,
+        this.pinNamedMap['Terminal 1']
+      );
+    }
+
+    if (this.pinNamedMap['IN3'].value > 0) {
+      this.pinNamedMap['Terminal 4'].setValue(
+        this.pinNamedMap['V IN'].value * (this.speedB / 5),
+        this.pinNamedMap['Terminal 4']
+      );
+    } else if (this.pinNamedMap['IN4'].value > 0) {
+      this.pinNamedMap['Terminal 3'].setValue(
+        this.pinNamedMap['V IN'].value * (this.speedB / 5),
+        this.pinNamedMap['Terminal 3']
+      );
+    } else {
+      this.pinNamedMap['Terminal 3'].setValue(
+        0,
+        this.pinNamedMap['Terminal 3']
+      );
+    }
+  }
+  /**
+   * Function provides component details
+   * @param keyName Unique Class name
+   * @param id Component id
+   * @param body body of property box
+   * @param title Component title
+   */
+  properties(): { keyName: string; id: number; body: HTMLElement; title: string; } {
+    const body = document.createElement('div');
+    body.innerText = 'If you Don\'t Connect The ENA and ENB Pins it automatically connects to the 5V suppy';
+    return {
+      keyName: this.keyName,
+      id: this.id,
+      body,
+      title: 'Motor Driver (L293D)'
+    };
+  }
+  /**
+   * Return the node which is connected to arduino
+   * @param node The Node which need to be checked
+   */
+  private getArduino(node: Point) {
+    if (
+      node.connectedTo &&
+      node.connectedTo.start &&
+      node.connectedTo.start.parent.keyName === 'ArduinoUno'
+    ) {
+      return node.connectedTo.start;
+    }
+    if (
+      node.connectedTo &&
+      node.connectedTo.end &&
+      node.connectedTo.end.parent.keyName === 'ArduinoUno'
+    ) {
+      return node.connectedTo.end;
+    }
+    return null;
+  }
+  /**
+   * Called on Start Simulation
+   */
+  initSimulation(): void {
+    const arduinoEnd: any = this.getArduino(this.pinNamedMap['ENB']);
+    if (arduinoEnd) {
+      const arduino = arduinoEnd.parent;
+      (arduino as ArduinoUno).addPWM(arduinoEnd, (v, p) => {
+        this.speedA = v / 100;
+        this.update();
+      });
+    }
+
+    const arduinoEnd1: any = this.getArduino(this.pinNamedMap['ENA']);
+    if (arduinoEnd1) {
+      const arduino = arduinoEnd1.parent;
+      (arduino as ArduinoUno).addPWM(arduinoEnd1, (v, p) => {
+        this.speedB = v / 100;
+        this.update();
+      });
+    }
+
+  }
+  /**
+   * Called on Stop Simulation
+   */
+  closeSimulation(): void {
+    this.pinNamedMap['IN1'].value = -1;
+    this.pinNamedMap['IN2'].value = -1;
+    this.pinNamedMap['IN3'].value = -1;
+    this.pinNamedMap['IN4'].value = -1;
+    this.speedA = 5;
+    this.speedB = 5;
+    this.prevValues = {
+      IN1: -1,
+      IN2: -1,
+      IN3: -1,
+      IN4: -1
+    };
+  }
+}
+
---
+++ Changes Made by Me/Utils.ts
@@ -0,0 +1,185 @@
+import { Buzzer } from './outputs/Buzzer';
+import { Battery9v, CoinCell } from './Battery';
+import { PushButton, SlideSwitch } from './inputs/Buttons';
+import { ArduinoUno } from './outputs/Arduino';
+import { LED, RGBLED } from './outputs/Led';
+import { UltrasonicSensor } from './inputs/UltrasonicSensor';
+import { PIRSensor } from './inputs/PIRSensor';
+import { Motor, L298N, ServoMotor } from './outputs/Motors';
+import { LCD16X2, SevenSegment } from './outputs/Display';
+import { Label } from './Miscellaneous';
+import { PhotoResistor } from './inputs/PhotoResistor';
+import { TMP36 } from './inputs/TemperatureSensors';
+import { Potentiometer } from './inputs/Potentiometer';
+import { Relay } from './inputs/Relay';
+import { MQ2 } from './inputs/GasSensor';
+import { Resistor, BreadBoard } from './General';
+import { DHT11 } from './inputs/DHT11';
+import { L293D } from './outputs/L293D';
+
+
+/**
+ * Utils class
+ * Contains All components with their section
+ */
+export class Utils {
+  /**
+   * Stores an object required by the Side Component Panel
+   */
+  static componentBox = {
+    input: [
+      ['PushButton', 'UltrasonicSensor', 'PIRSensor'], // Row
+      ['SlideSwitch', 'MQ2', 'TMP36'],
+      ['PotentioMeter' , 'DHT11' /* 'PhotoResistor'*/]
+    ],
+    power: [
+      ['Battery9v', 'CoinCell'] // Row
+    ],
+    controllers: [
+      ['ArduinoUno'] // Row
+    ],
+    output: [
+      ['Buzzer', 'LED', 'Motor'], // Row
+      ['RGBLED', 'ServoMotor', 'SevenSegment'],
+      ['LCD16X2']
+    ],
+    drivers: [
+      ['L298N', 'L293D' ]
+    ],
+    misc: [
+      ['Label', 'RelayModule']
+    ],
+    general: [
+      ['Resistor', 'BreadBoard']
+    ]
+  };
+
+  /** Components with thier name, image, classname */
+  static components = {
+    BreadBoard: {
+      name: 'BreadBoard',
+      image: './assets/images/components/Breadboard.svg',
+      className: BreadBoard
+    },
+    Resistor: {
+      name: 'Resistor',
+      image: './assets/images/components/Resistor.png',
+      className: Resistor
+    },
+    RGBLED: {
+      name: 'RGB LED',
+      image: './assets/images/components/RGBLED.png',
+      className: RGBLED
+    },
+    SevenSegment: {
+      name: 'Seven Segment Display',
+      image: './assets/images/components/SevenSegment.png',
+      className: SevenSegment
+    },
+    MQ2: {
+      name: 'Gas Sensor MQ2',
+      image: './assets/images/components/GasSensor.svg',
+      className: MQ2
+    },
+    ServoMotor: {
+      name: 'Servo Motor',
+      image: './assets/images/components/Servo.png',
+      className: ServoMotor
+    },
+    RelayModule: {
+      name: 'Relay Module',
+      image: './assets/images/components/1ChannelRelay.svg',
+      className: Relay
+    },
+    PotentioMeter: {
+      name: 'Potentiometer',
+      image: './assets/images/components/Potentiometer.png',
+      className: Potentiometer
+    },
+    TMP36: {
+      name: 'Temperature Sensor TMP36',
+      image: './assets/images/components/TMP36.svg',
+      className: TMP36
+    },
+    DHT11: {
+      name: 'DHT11',
+      image: './assets/images/components/DHT11.svg',
+      className: DHT11
+    },
+    PhotoResistor: {
+      name: 'Photo Resistor',
+      image: './assets/images/components/PhotoResistor.svg',
+      className: PhotoResistor
+    },
+    Label: {
+      name: 'Label',
+      image: './assets/images/components/Text.png',
+      className: Label
+    },
+    CoinCell: {
+      name: 'Coin Cell 3V',
+      image: './assets/images/components/CoinCell.svg',
+      className: CoinCell
+    },
+    SlideSwitch: {
+      name: 'Slide Switch',
+      image: './assets/images/components/SlideSwitch.png',
+      className: SlideSwitch
+    },
+    LCD16X2: {
+      name: 'LCD 16x2',
+      image: './assets/images/components/LCD16X2.png',
+      className: LCD16X2
+    },
+    L298N: {
+      name: 'Motor Driver L298N',
+      image: './assets/images/components/L298N.png',
+      className: L298N
+    },
+    L293D: {
+      name: 'Motor Driver L293D',
+      image: './assets/images/components/L293D.png',
+      className: L293D
+    },
+    Motor: {
+      name: 'Motor',
+      image: './assets/images/components/Motor.png',
+      className: Motor
+    },
+    PIRSensor: {
+      name: 'PIR Sensor',
+      image: './assets/images/components/PIRSensor.png',
+      className: PIRSensor
+    },
+    UltrasonicSensor: {
+      name: 'Ultrasonic Distance Sensor',
+      image: './assets/images/components/UltrasonicSensor.png',
+      className: UltrasonicSensor
+    },
+    LED: {
+      name: 'LED',
+      image: './assets/images/components/led.png',
+      className: LED
+    },
+    ArduinoUno: {
+      name: 'Arduino UNO',
+      image: './assets/images/components/ArduinoUno.png',
+      className: ArduinoUno
+    },
+    PushButton: {
+      name: 'Push Button',
+      image: './assets/images/components/PushButton.png',
+      className: PushButton
+    },
+    Battery9v: {
+      name: '9v Battery',
+      image: './assets/images/components/Battery9v.png',
+      className: Battery9v
+    },
+    Buzzer: {
+      name: 'Buzzer',
+      image: './assets/images/components/Buzzer.png',
+      className: Buzzer
+    }
+  };
+}
