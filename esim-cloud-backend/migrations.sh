e879b5903f1b2305e2ce3c5abbb4e4866dacb1ce
Rugz007 <dsweb007@gmail.com>
2021-06-07 15:12:23 +0530

    Fixed migrations.sh

--- esim-cloud-backend/migrations.sh
+++ esim-cloud-backend/migrations.sh
@@ -4,15 +4,16 @@ python manage.py makemigrations workflowAPI
 python manage.py migrate workflowAPI
 python manage.py makemigrations publishAPI
 python manage.py migrate publishAPI
+python manage.py makemigrations libAPI
+python manage.py migrate libAPI
 python manage.py makemigrations saveAPI
 python manage.py migrate saveAPI
 python manage.py makemigrations simulationAPI
 python manage.py migrate simulationAPI --database="mongodb"
-python manage.py makemigrations libAPI
-python manage.py migrate libAPI
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+ python manage.py loaddata initial_setup
 python manage.py createsuperuser_noinput --username=admin --password=admin
 python manage.py load_default_libs --username=admin --location=kicad-symbols/default/ --default
 python manage.py load_default_libs --username=admin --location=kicad-symbols/additional/
