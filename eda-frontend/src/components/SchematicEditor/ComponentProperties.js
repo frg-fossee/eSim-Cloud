01ecafa13b4ede392fb010ba02469f9e28e5e7c8
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-06-24 20:43:12 +0530

    Made the Component Properties draggable

--- eda-frontend/src/components/SchematicEditor/ComponentProperties.js
+++ eda-frontend/src/components/SchematicEditor/ComponentProperties.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import disableScroll from 'disable-scroll';
 import { useSelector, useDispatch } from 'react-redux'
 import { setCompProperties } from '../../redux/actions/index'
+import Draggable from 'react-draggable'
 import { ListItem, ListItemText, Button, TextField, TextareaAutosize, Paper } from '@material-ui/core'
 
 export default function ComponentProperties () {
@@ -20,7 +21,7 @@ export default function ComponentProperties () {
     if(isOpen){
       var temp = document.getElementById("properties-modal").clientHeight
       console.log(temp,screen.height,y)
-      if(screen.height - 260 - y < temp){
+      if(0.6*screen.height - 260 - y < temp){
         if(temp+100>=y)
           y = 100
         else
@@ -54,62 +55,55 @@ export default function ComponentProperties () {
   }
 
   return (
-
-    <div id="properties-modal" style={isOpen ? {position:'absolute', left: `${x}px`, top: `${height}px`} : { display: 'none' }}>
-      <Paper>
-
-      <ListItem>
-        <ListItemText primary='Component Properties' secondary={properties.NAME} />
-      </ListItem>
-
-      {
-        Object.keys(properties).map((keyName, i) => {
-          if (keyName === 'MODEL') {
-            return <ListItem key={i}>
-              <TextareaAutosize id={keyName} label={keyName} value={val[keyName] || ''} rowsMin={4} aria-label={keyName} onChange={getInputValues} placeholder={keyName} style={{ width: '100%' }} />
-            </ListItem>
-            // eslint-disable-next-line brace-style
-          }
-
-          else if (keyName === 'EXTRA_EXPRESSION') {
-            return <ListItem key={i}>
-              <TextareaAutosize id={keyName} label={keyName} value={val[keyName] || ''} rowsMin={4} aria-label={keyName} onChange={getInputValues} placeholder={'EXPRESSION'} style={{ width: '100%' }} />
-            </ListItem>
-          }
-
-          //
-          else if (keyName.charAt(0) === 'N' && keyName !== 'NAME') {
-            return <span key={i} />
-          } else if (keyName.includes('UNIT')) {
-            return <span key={i} />
-          }
-          else if (keyName === 'PREFIX') {
+    isOpen &&
+    <div id="properties-modal" style={{position:'absolute', left: `${x}px`, top: `${height}px`}}>
+      <Draggable>
+        <Paper>
+        <ListItem>
+          <ListItemText primary='Component Properties' secondary={properties.NAME} />
+        </ListItem>
+        {
+          Object.keys(properties).map((keyName, i) => {
+            if (keyName === 'MODEL') {
+              return <ListItem key={i}>
+                <TextareaAutosize rowsMax={4} id={keyName} label={keyName} value={val[keyName] || ''} rowsMin={4} aria-label={keyName} onChange={getInputValues} placeholder={keyName} style={{ width: '100%' }} />
+              </ListItem>
+              // eslint-disable-next-line brace-style
+            }
+            else if (keyName === 'EXTRA_EXPRESSION') {
+              return <ListItem key={i}>
+                <TextareaAutosize rowsMax={4} id={keyName} label={keyName} value={val[keyName] || ''} rowsMin={4} aria-label={keyName} onChange={getInputValues} placeholder={'EXPRESSION'} style={{ width: '100%' }} />
+              </ListItem>
+            }
+            else if (keyName.charAt(0) === 'N' && keyName !== 'NAME') {
+              return <span key={i} />
+            } else if (keyName.includes('UNIT')) {
+              return <span key={i} />
+            }
+            else if (keyName === 'PREFIX') {
+              return (
+                <ListItem key={i}>
+                  <TextField id={keyName} label='LABEL' value={val[keyName] || ''} size='small' variant="outlined" onChange={getInputValues} />
+                </ListItem>)
+            }
+            else if (keyName === 'NAME') {
+              return (
+                <ListItem key={i}>
+                  <TextField disabled id={keyName} label='COMPONENT NAME' value={val[keyName] || ''} size='small' variant="outlined" onChange={getInputValues} />
+                </ListItem>)
+            }
             return (
               <ListItem key={i}>
-                <TextField id={keyName} label='LABEL' value={val[keyName] || ''} size='small' variant="outlined" onChange={getInputValues} />
+                <TextField id={keyName} label={keyName} value={val[keyName] || ''} size='small' variant="outlined" onChange={getInputValues} />
+                {val[`${keyName}_UNIT`] && <span style={{ marginLeft: '10px' }}>{val[`${keyName}_UNIT`] || ''}</span>}
               </ListItem>)
-          }
-          else if (keyName === 'NAME') {
-            return (
-              <ListItem key={i}>
-                <TextField disabled id={keyName} label='COMPONENT NAME' value={val[keyName] || ''} size='small' variant="outlined" onChange={getInputValues} />
-
-              </ListItem>)
-          }
-
-          return (
-            <ListItem key={i}>
-              <TextField id={keyName} label={keyName} value={val[keyName] || ''} size='small' variant="outlined" onChange={getInputValues} />
-              {val[`${keyName}_UNIT`] && <span style={{ marginLeft: '10px' }}>{val[`${keyName}_UNIT`] || ''}</span>}
-            </ListItem>)
-        })
-      }
-
-      <ListItem>
-        <Button size='small' variant="contained" color="primary" onClick={setProps}>SET PARAMETERS</Button>
-      </ListItem>
-      </Paper>
-
+          })
+        }
+        <ListItem>
+          <Button size='small' variant="contained" color="primary" onClick={setProps}>SET PARAMETERS</Button>
+        </ListItem>
+        </Paper>
+      </Draggable>
     </div>
   )
 }
