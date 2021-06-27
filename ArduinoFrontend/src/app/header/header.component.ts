e6d12223feeceaa0000064aeb6b9e3c05b0e3afe
asif <asifmulla166037@gmail.com>
2021-06-27 20:57:03 +0530

    removing comment on header file

--- ArduinoFrontend/src/app/header/header.component.ts
+++ ArduinoFrontend/src/app/header/header.component.ts
@@ -46,7 +46,6 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     // Get Login Token
     this.token = Login.getToken();
-    // If token is available then get username
     if (this.token) {
       this.api.userInfo(this.token).subscribe((v) => {
         this.username = v.username;
