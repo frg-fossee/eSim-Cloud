92e23f5b858f0ee090417b8cb284f236f664cc5e
Rugz007 <dsweb007@gmail.com>
2021-08-20 16:30:15 +0530

    Improved Submit Screen UI

--- eda-frontend/src/components/LTI/SubmitResults.js
+++ eda-frontend/src/components/LTI/SubmitResults.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Paper,
   Dialog,
@@ -7,36 +7,43 @@ import {
   Slide,
   AppBar,
   Toolbar,
-  IconButton
-} from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import PropTypes from 'prop-types'
-import CancelIcon from '@material-ui/icons/Cancel'
-import CheckCircleIcon from '@material-ui/icons/CheckCircle'
-import CloseIcon from '@material-ui/icons/Close'
-import CompareGraph from './CompareGraph'
+  IconButton,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import CancelIcon from "@material-ui/icons/Cancel";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import CloseIcon from "@material-ui/icons/Close";
+import CompareGraph from "./CompareGraph";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    position: 'relative'
+    position: "relative",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    backgroundColor: '#404040',
+    color: '#fff'
   }
-}))
+}));
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />
-})
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 function SubmitResults({ show, setResults, results }) {
-  const classes = useStyles()
+  const classes = useStyles();
   const showIcons = (item) => {
     if (
-      results.comparison_result === 'Same Values' || results.comparison_result.same.includes(item)
+      results.comparison_result === "Same Values" ||
+      results.comparison_result.same.includes(item)
     ) {
-      return <CheckCircleIcon />
+      return <CheckCircleIcon />;
     } else {
-      return <CancelIcon />
+      return <CancelIcon />;
     }
-  }
+  };
   return (
     <Dialog
       open={show}
@@ -61,31 +68,50 @@ function SubmitResults({ show, setResults, results }) {
           <h3>Your Score: {results.score}</h3>
           {/* <h2>Teacher Values</h2> */}
         </Grid>
-        <Grid item xs={12}>
-          {(results.given && results.given.graph !== 'true') ? (
-            <>
-              <h2>Your Submission Values: </h2>
-              {results.given.data.map((ele) => (
+        {results.expected && results.given.graph !== "true" && (
+          <>
+            <Grid item xs={5}>
+              <h2>Teacher's values:</h2>
+              {results.expected.data.map((ele, index) => (
                 <Paper key={ele}>
                   <Typography>
-                    {showIcons(ele[0])}
-                    {ele[0]} : {ele[2]}
+                    {index + 1}. {ele[0]} : {ele[2]}
+                  </Typography>
+                </Paper>
+              ))}
+            </Grid>
+            <Grid item xs={2} />
+          </>
+        )}
+        <Grid
+          item
+          xs={results.expected && results.given.graph !== "true" ? 5 : 12}
+        >
+          {results.given && results.given.graph !== "true" ? (
+            <>
+              <h2>Your Submission Values: </h2>
+              {results.given.data.map((ele, index) => (
+                <Paper key={ele}>
+                  {showIcons(ele[0])}
+                  <Typography>
+                    {index + 1}. {ele[0]} : {ele[2]}
                   </Typography>
                 </Paper>
               ))}
             </>
-          ) : <CompareGraph expected={results.expected} given={results.given} />
-          }
+          ) : (
+            <CompareGraph expected={results.expected} given={results.given} />
+          )}
         </Grid>
       </Grid>
     </Dialog>
-  )
+  );
 }
 
-export default SubmitResults
+export default SubmitResults;
 
 SubmitResults.propTypes = {
   show: PropTypes.bool,
   setResults: PropTypes.func,
-  results: PropTypes.object
-}
+  results: PropTypes.object,
+};
