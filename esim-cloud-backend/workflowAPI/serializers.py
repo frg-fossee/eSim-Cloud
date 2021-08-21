8e18ad1ed7b83590b75cc8aa10e71e0cfda3c3b6
asif <asifmulla166037@gmail.com>
2021-08-21 16:07:03 +0530

    pep8 issues 2

--- esim-cloud-backend/workflowAPI/serializers.py
+++ esim-cloud-backend/workflowAPI/serializers.py
@@ -8,10 +8,11 @@ class UserRoleRetreieveSerializer(serializers.ModelSerializer):
     arduino_reviewer = serializers.BooleanField()
     is_arduino_staff = serializers.BooleanField()
     is_esim_staff = serializers.BooleanField()
+
     class Meta:
         model = CustomGroup
-        fields = ['group', 'is_type_reviewer', 'arduino_reviewer', 'e_sim_reviewer', 
-        'is_type_staff', 'is_arduino_staff', 'is_esim_staff']
+        fields = ['group', 'is_type_reviewer', 'arduino_reviewer',
+                  'e_sim_reviewer', 'is_type_staff', 'is_arduino_staff', 'is_esim_staff']
 
 
 class StatusWithNotesSerializer(serializers.ModelSerializer):
