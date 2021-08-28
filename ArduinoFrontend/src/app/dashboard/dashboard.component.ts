acdb68377b6bb29e62e83d1fd51276e915f16a85
CHAITANYA-IN <chaitanyashinge@gmail.com>
2021-08-28 16:39:10 +0530

    Add LTI Apps tab in dashboard and delete LTI app option

--- ArduinoFrontend/src/app/dashboard/dashboard.component.ts
+++ ArduinoFrontend/src/app/dashboard/dashboard.component.ts
@@ -525,10 +525,28 @@ export class DashboardComponent implements OnInit {
     return `${window.location.protocol}\\\\${window.location.host}/arduino/#/project/${slug}`;
   }
 
-  DisableLTI() {
+  DeleteLTI(selected) {
     const token = Login.getToken();
     if(token) {
-      
+      AlertService.showConfirm('Are You Sure You want to Delete LTI App', () => {
+        this.api.removeLTIDetails(selected.id, token).subscribe(_ => {
+          selected.lti_id = null;
+          AlertService.showAlert("Deleted LTI App successfully");
+          this.readOnCloudItems();
+        }, err => {
+          AlertService.showAlert("Something went wrong");
+          console.log(err);
+        });
+      });
     }
+  }
+
+  getLTIApps() {
+    this.online.forEach((circuit, index) => {
+      if(circuit.lti_id) {
+        circuit.index = index;
+      }
+    });
+    return this.online.filter(circuit => circuit.lti_id);
   }
 }
