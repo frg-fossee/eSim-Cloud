80692dad4695f4e56709cce8471098147529f4a0
deepampriyadarshi <deepam.odhisha@gmail.com>
2022-03-02 15:44:33 +0530

    Adding C inline assembly support

--- esim-cloud-backend/arduinoAPI/tasks.py
+++ esim-cloud-backend/arduinoAPI/tasks.py
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 PATTERN = r'^[ \t]*\w+\d*[ \t]+\w+\d*\(([ \t]*\w+\d*[ \t]+\w+\d*[ \t]*\,?)*\)[ \t]*\n?\{'  # noqa
 
 
-def saveFiles(data):
+def saveFiles(data, langIndex):
     # try:
     filenames = []
     if not os.path.exists(settings.MEDIA_ROOT):
@@ -27,7 +27,10 @@ def saveFiles(data):
 
         Path(work_dir).mkdir(parents=True, exist_ok=True)
 
-        filename = settings.MEDIA_ROOT+'/'+str(foldername)+'/sketch.ino'
+        if langIndex == 0:
+            filename = settings.MEDIA_ROOT+'/'+str(foldername)+'/sketch.ino'
+        elif langIndex == 1:
+            filename = settings.MEDIA_ROOT+'/'+str(foldername)+'/sketch.c'
 
         fout = open(filename, 'w', encoding='utf8')
         matches = re.finditer(PATTERN, data.get(k, ''), re.MULTILINE)
@@ -108,18 +111,77 @@ def CompileINO(filenames):
             logger.info(parent)
     return ret
 
+def CompileInlineAssembly(filenames):
+    ret = {}
+    try:
+        for filename in filenames:
+            c_name = settings.MEDIA_ROOT+'/'+str(filename)+'/sketch.c'
+            obj_name = settings.MEDIA_ROOT+'/'+str(filename)+'/sketch.o'
+            bin_name = settings.MEDIA_ROOT+'/'+str(filename)+'/sketch'
+            out_name = settings.MEDIA_ROOT+'/'+str(filename)+'/out.hex'
+            logger.info('Compiling')
+            logger.info(c_name)
+
+            ps = subprocess.Popen(
+                "avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o " + obj_name + " " + c_name + " && avr-gcc -mmcu=atmega328p " + obj_name + " -o " + bin_name + " && avr-objcopy -O ihex -R .eeprom " + bin_name + " " + out_name,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                shell=True
+            )
+            output, err = ps.communicate()
+            # print(ps.returncode)
+            # print(output)
+            # print(err)
+            data = ''
+            if err == '' and ps.returncode != 0:
+                err = b'Code Cannot be Compiled: Unknown Reason'
+
+            if os.path.isfile(out_name):
+                data = open(out_name, 'r').read()
+            # print(data)
+            pos = filename.find('_')
+            if pos != -1:
+                pos += 1
+                key = filename[pos:]
+            else:
+                key = filename
+
+            ret[key] = {
+                'output': "Compiled Successfully",
+                'error': re.sub(
+                    rf'{settings.MEDIA_ROOT}/{filename}/',
+                    '',
+                    err.decode('utf-8')
+                ),
+                'data': data
+            }
+
+    except Exception:
+        print(traceback.format_exc())
+        return False
+    finally:
+        for filename in filenames:
+            parent = settings.MEDIA_ROOT+'/'+str(filename)
+            shutil.rmtree(parent, True)
+            logger.info('Removing')
+            logger.info(parent)
+    return ret
+
 
 @shared_task
-def compile_sketch_task(task_id, data):
+def compile_sketch_task(task_id, data, langIndex):
     try:
         current_task.update_state(
             state='PROGRESS',
             meta={'current_process': 'Saving Files'})
-        filenames = saveFiles(data)
+        filenames = saveFiles(data, langIndex)
         current_task.update_state(
             state='PROGRESS',
             meta={'current_process': 'Starting Compiling'})
-        output = CompileINO(filenames)
+        if langIndex == 0:
+            output = CompileINO(filenames)
+        elif langIndex == 1:
+            output = CompileInlineAssembly(filenames)
         if isinstance(output, bool):
             current_task.update_state(state='FAILURE', meta={
                 'exc_type': 'Compilation Error',
