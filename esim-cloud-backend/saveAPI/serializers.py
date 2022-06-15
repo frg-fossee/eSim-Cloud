425bb84cca444b86557d4b824f9c29003736ab14
deepampriyadarshi <deepam.odhisha@gmail.com>
2022-06-15 13:25:36 +0530

    Fixing Arduino LTI app view page

--- esim-cloud-backend/saveAPI/serializers.py
+++ esim-cloud-backend/saveAPI/serializers.py
@@ -6,7 +6,7 @@ from saveAPI.models import StateSave, Gallery, ArduinoModelSimulationData
 from libAPI.models import Library
 from libAPI.serializers import LibrarySerializer
 from django.core.files.base import ContentFile
-from ltiAPI.models import lticonsumer
+from ltiAPI.models import ArduinLTIConsumer, lticonsumer
 import base64
 import six
 import uuid
@@ -55,10 +55,13 @@ class StateSaveSerializer(serializers.ModelSerializer):
                   'id', 'lti_id', 'is_submission')
 
     def get_lti_id(self, obj):
-        save_id = obj.save_id
-        ltis = lticonsumer.objects.filter(model_schematic__save_id=save_id)
-        if ltis.exists():
-            return ltis[0].id
+        save_id = obj.save_id                                              
+        ltis = lticonsumer.objects.filter(model_schematic__save_id=save_id)             
+        arduinoLTIs = ArduinLTIConsumer.objects.filter(model_schematic__save_id=save_id)
+        if ltis.exists():    
+            return ltis[0].id     
+        elif arduinoLTIs.exists():  
+            return arduinoLTIs[0].id
         else:
             return None
 
@@ -85,10 +88,13 @@ class SaveListSerializer(serializers.ModelSerializer):
                   'is_submission')
 
     def get_lti_id(self, obj):
-        save_id = obj.save_id
-        ltis = lticonsumer.objects.filter(model_schematic__save_id=save_id)
-        if ltis.exists():
-            return ltis[0].id
+        save_id = obj.save_id                                              
+        ltis = lticonsumer.objects.filter(model_schematic__save_id=save_id)             
+        arduinoLTIs = ArduinLTIConsumer.objects.filter(model_schematic__save_id=save_id)
+        if ltis.exists():    
+            return ltis[0].id     
+        elif arduinoLTIs.exists():  
+            return arduinoLTIs[0].id
         else:
             return None
 
