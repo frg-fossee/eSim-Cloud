437409a238b7334664c843861eb7c9217cc5651a
Mishtee Gandhi <mishtee13@gmail.com>
2023-12-21 16:53:44 +0530

    Change font size in code editor

--- ArduinoFrontend/src/app/code-editor/code-editor.component.ts
+++ ArduinoFrontend/src/app/code-editor/code-editor.component.ts
@@ -26,9 +26,11 @@ export class CodeEditorComponent {
   /**
    * Monaco code editor options
    */
+   size = 15
   editorOptions = {
     theme: 'vs',
-    language: 'c'
+    language: 'c',
+    fontSize: 15
   };
   /**
    * Instance of Monaco editor
@@ -144,6 +146,20 @@ export class CodeEditorComponent {
       }
     }
   }
+  /**
+  * Increase the size of the font in the editor
+  */
+  IncreaseFont(fontSize: number){ 
+    this.size = this.size + 1;
+    this.editorOptions = {...this.editorOptions, fontSize:this.size};
+  }
+  /**
+  * Decrease the size of the font in the editor
+  */
+  DecreaseFont(fontSize: number){ 
+    this.size = this.size - 1;
+    this.editorOptions = {...this.editorOptions, fontSize:this.size};
+  }  
   /**
    * Download the code from code editor
    */
